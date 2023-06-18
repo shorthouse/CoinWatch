4e5f050bff543dad08520b7042f73f3f29592c89
Harry S (Macbook) <shorthouse20@gmail.com>
2023-06-18 12:38:01 +0100

    Add crypto list view model

---
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/screen/list/ListViewModel.kt
@@ -0,0 +1,51 @@
+package dev.shorthouse.cryptodata.ui.screen.list
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.shorthouse.cryptodata.common.Resource
+import dev.shorthouse.cryptodata.domain.GetCryptocurrenciesUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class ListViewModel @Inject constructor(
+    private val getCryptocurrenciesUseCase: GetCryptocurrenciesUseCase,
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(ListUiState())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        getCryptocurrencies()
+    }
+
+    private fun getCryptocurrencies() {
+        getCryptocurrenciesUseCase().onEach { result ->
+            _uiState.update {
+                when (result) {
+                    is Resource.Loading -> {
+                        it.copy(
+                            isLoading = true,
+                        )
+                    }
+
+                    is Resource.Success -> {
+                        it.copy(
+                            cryptocurrencies = result.data ?: emptyList(),
+                        )
+                    }
+
+                    is Resource.Error -> {
+                        it.copy(
+                            error = result.message ?: "An unexpected error occurred",
+                        )
+                    }
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
