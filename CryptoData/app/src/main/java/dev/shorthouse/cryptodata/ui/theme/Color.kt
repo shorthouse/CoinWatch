3832ab5bb1cd936307bd59d731d6433a0a197621
Harry S (Macbook) <shorthouse20@gmail.com>
2023-06-19 20:48:54 +0100

    Add green and red price change colours

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/theme/Color.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/theme/Color.kt
@@ -2,10 +2,17 @@ package dev.shorthouse.cryptodata.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
+// Light
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val light_onSurfaceVariant = Color(0xFF767680)
+
+// Dark
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+val dark_onSurfaceVariant = Color(0xFF8F909A)
+
+val Green = Color(0xFF2E7D32)
+val Red = Color(0xFFB00020)
