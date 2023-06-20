9d1a1978317ee19690ee1a7aec51525056a1f82e
Harry S (Macbook) <shorthouse20@gmail.com>
2023-06-20 19:49:19 +0100

    Add loading indicator composable component

---
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/component/LoadingIndicator.kt
@@ -0,0 +1,21 @@
+package dev.shorthouse.cryptodata.ui.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoadingIndicator() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator(
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
