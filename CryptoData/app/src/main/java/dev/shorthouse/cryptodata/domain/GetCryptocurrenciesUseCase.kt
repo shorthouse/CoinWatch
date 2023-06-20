89bab204952b14a9c34e1ebe9e1e34c92bd9a32e
Harry S (Macbook) <shorthouse20@gmail.com>
2023-06-20 19:50:26 +0100

    Rename to coin

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/domain/GetCryptocurrenciesUseCase.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/domain/GetCryptocurrenciesUseCase.kt
@@ -1,24 +1,24 @@
 package dev.shorthouse.cryptodata.domain
 
 import dev.shorthouse.cryptodata.common.Resource
-import dev.shorthouse.cryptodata.data.repository.CryptocurrencyRepository
+import dev.shorthouse.cryptodata.data.repository.CoinRepository
 import dev.shorthouse.cryptodata.di.IoDispatcher
-import dev.shorthouse.cryptodata.model.Cryptocurrency
+import dev.shorthouse.cryptodata.model.Coin
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class GetCryptocurrenciesUseCase @Inject constructor(
-    private val cryptocurrencyRepository: CryptocurrencyRepository,
+    private val coinRepository: CoinRepository,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
 ) {
-    suspend operator fun invoke(): Flow<Resource<List<Cryptocurrency>>> {
+    suspend operator fun invoke(): Flow<Resource<List<Coin>>> {
         return getCryptocurrencies()
     }
 
-    private suspend fun getCryptocurrencies(): Flow<Resource<List<Cryptocurrency>>> {
-        return cryptocurrencyRepository.getCryptocurrencies()
+    private suspend fun getCryptocurrencies(): Flow<Resource<List<Coin>>> {
+        return coinRepository.getCryptocurrencies()
             .flowOn(ioDispatcher)
     }
 }
