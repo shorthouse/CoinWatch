e23603581f9b3ddc37377da74caf6c082a07c1b2
Harry S (Macbook) <shorthouse20@gmail.com>
2023-06-20 19:50:14 +0100

    Rename to Coin

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/screen/list/ListUiState.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/screen/list/ListUiState.kt
@@ -1,9 +1,9 @@
 package dev.shorthouse.cryptodata.ui.screen.list
 
-import dev.shorthouse.cryptodata.model.Cryptocurrency
+import dev.shorthouse.cryptodata.model.Coin
 
 data class ListUiState(
-    val cryptocurrencies: List<Cryptocurrency> = emptyList(),
+    val cryptocurrencies: List<Coin> = emptyList(),
     val isLoading: Boolean = false,
     val error: String? = null,
 )
