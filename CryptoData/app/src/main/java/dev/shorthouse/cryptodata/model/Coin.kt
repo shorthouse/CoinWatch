c8b2813579414a477177821df10b60a1189d1bd9
Harry S (Macbook) <shorthouse20@gmail.com>
2023-06-20 19:49:00 +0100

    Rename to Coin

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/model/Coin.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/model/Coin.kt
@@ -2,13 +2,13 @@ package dev.shorthouse.cryptodata.model
 
 import com.google.gson.annotations.SerializedName
 
-data class Cryptocurrency(
+data class Coin(
     val id: String,
     val symbol: String,
     val name: String,
     val image: String,
     @SerializedName("current_price")
-    val currentPrice: String,
+    val currentPrice: Double,
     @SerializedName("price_change_percentage_24h")
-    val priceChange: String,
+    val priceChangePercentage: Double,
 )
