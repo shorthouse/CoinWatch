d799a216682994f8633d2e1012c5c0cec3b17322
Harry S (Macbook) <shorthouse20@gmail.com>
2023-06-21 22:32:39 +0100

    Add historic data to coin detail

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/model/CoinDetail.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/model/CoinDetail.kt
@@ -7,16 +7,24 @@ data class CoinDetail(
     val image: String,
     val currentPrice: Double,
     val priceChangePercentage: Double,
+
+    val marketCap: Long,
+    val marketCapChangePercentage: Double,
+    val marketCapRank: Int,
+    val dailyHigh: Double,
+    val dailyHighChangePercentage: Double,
+    val dailyLow: Double,
+    val dailyLowChangePercentage: Double,
+
     val genesisDate: String,
-//    val allTimeHigh: String,
-//    val allTimeHighDate: String,
-//    val allTimeLow: String,
-//    val allTimeLowDate: String,
-//    val marketCap: Long,
-//    val marketCapChangePercentage: String,
-//    val marketCapRank: Int,
+    //    val allTimeHigh: String,
+    //    val allTimeHighDate: String,
+    //    val allTimeLow: String,
+    //    val allTimeLowDate: String,
+
     val description: String,
+
     val homepageLink: String?,
-    val blockchainLink: String?,
+    val githubLink: String?,
     val subredditLink: String?
 )
