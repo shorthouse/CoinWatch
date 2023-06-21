a4b5f5e8bdd02b286a0e16de068eacd2e9673b82
Harry S (Macbook) <shorthouse20@gmail.com>
2023-06-21 22:33:30 +0100

    Change to use .launchIn()

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/screen/list/ListViewModel.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/screen/list/ListViewModel.kt
@@ -4,18 +4,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.shorthouse.cryptodata.common.Resource
-import dev.shorthouse.cryptodata.domain.GetCryptocurrenciesUseCase
+import dev.shorthouse.cryptodata.domain.GetCoinsUseCase
+import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @HiltViewModel
 class ListViewModel @Inject constructor(
-    private val getCryptocurrenciesUseCase: GetCryptocurrenciesUseCase,
+    private val getCryptocurrenciesUseCase: GetCoinsUseCase
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(ListUiState())
     val uiState = _uiState.asStateFlow()
@@ -25,31 +24,29 @@ class ListViewModel @Inject constructor(
     }
 
     private fun getCryptocurrencies() {
-        viewModelScope.launch {
-            getCryptocurrenciesUseCase().onEach { result ->
-                _uiState.update {
-                    when (result) {
-                        is Resource.Loading -> {
-                            it.copy(
-                                isLoading = true,
-                            )
-                        }
+        getCryptocurrenciesUseCase().onEach { result ->
+            _uiState.update {
+                when (result) {
+                    is Resource.Loading -> {
+                        it.copy(
+                            isLoading = true
+                        )
+                    }
 
-                        is Resource.Success -> {
-                            it.copy(
-                                cryptocurrencies = result.data ?: emptyList(),
-                                isLoading = false,
-                            )
-                        }
+                    is Resource.Success -> {
+                        it.copy(
+                            cryptocurrencies = result.data ?: emptyList(),
+                            isLoading = false
+                        )
+                    }
 
-                        is Resource.Error -> {
-                            it.copy(
-                                error = result.message ?: "An unexpected error occurred",
-                            )
-                        }
+                    is Resource.Error -> {
+                        it.copy(
+                            error = result.message ?: "An unexpected error occurred"
+                        )
                     }
                 }
-            }.launchIn(viewModelScope)
-        }
+            }
+        }.launchIn(viewModelScope)
     }
 }
