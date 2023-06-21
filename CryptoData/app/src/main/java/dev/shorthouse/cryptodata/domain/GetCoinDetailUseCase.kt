d63414be9bca1488eae27fb55252cc1a376e3461
Harry S (Macbook) <shorthouse20@gmail.com>
2023-06-21 22:33:20 +0100

    Remove unneeded suspend in flow function

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/domain/GetCoinDetailUseCase.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/domain/GetCoinDetailUseCase.kt
@@ -4,20 +4,20 @@ import dev.shorthouse.cryptodata.common.Resource
 import dev.shorthouse.cryptodata.data.repository.CoinRepository
 import dev.shorthouse.cryptodata.di.IoDispatcher
 import dev.shorthouse.cryptodata.model.CoinDetail
+import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
-import javax.inject.Inject
 
 class GetCoinDetailUseCase @Inject constructor(
     private val coinRepository: CoinRepository,
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) {
-    suspend operator fun invoke(): Flow<Resource<CoinDetail>> {
+    operator fun invoke(): Flow<Resource<CoinDetail>> {
         return getCoinDetail()
     }
 
-    private suspend fun getCoinDetail(): Flow<Resource<CoinDetail>> {
+    private fun getCoinDetail(): Flow<Resource<CoinDetail>> {
         return coinRepository.getCoinDetail()
             .flowOn(ioDispatcher)
     }
