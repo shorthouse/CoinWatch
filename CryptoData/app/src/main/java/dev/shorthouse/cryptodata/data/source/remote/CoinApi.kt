c7cba307bf0655e3615e6235d4257abb74acc5e7
Harry S (Macbook) <shorthouse20@gmail.com>
2023-06-21 22:34:08 +0100

    Rename to CoinApi

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/data/source/remote/CoinApi.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/data/source/remote/CoinApi.kt
@@ -5,10 +5,14 @@ import dev.shorthouse.cryptodata.data.source.remote.dto.CryptocurrencyDto
 import retrofit2.Response
 import retrofit2.http.GET
 
-interface CryptocurrencyApi {
-    @GET("coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h&locale=en&precision=2")
+interface CoinApi {
+    @GET(
+        "coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h&locale=en&precision=2"
+    )
     suspend fun getCryptocurrencies(): Response<List<CryptocurrencyDto>>
 
-    @GET("coins/ethereum?localization=false&tickers=false&community_data=false&developer_data=false")
+    @GET(
+        "coins/ethereum?localization=false&tickers=false&community_data=false&developer_data=false"
+    )
     suspend fun getCoinDetail(): Response<CoinDetailDto>
 }
