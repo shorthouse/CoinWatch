22443cf3a3b089db7df486047887acf5eeb6bed9
Harry S <shorthouse20@gmail.com>
2023-06-22 20:36:07 +0100

    Add sparkline data to GET coin detail

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/data/source/remote/CoinApi.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/data/source/remote/CoinApi.kt
@@ -1,7 +1,7 @@
 package dev.shorthouse.cryptodata.data.source.remote
 
 import dev.shorthouse.cryptodata.data.source.remote.dto.CoinDetailDto
-import dev.shorthouse.cryptodata.data.source.remote.dto.CryptocurrencyDto
+import dev.shorthouse.cryptodata.data.source.remote.dto.CoinDto
 import retrofit2.Response
 import retrofit2.http.GET
 
@@ -9,10 +9,10 @@ interface CoinApi {
     @GET(
         "coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h&locale=en&precision=2"
     )
-    suspend fun getCryptocurrencies(): Response<List<CryptocurrencyDto>>
+    suspend fun getCryptocurrencies(): Response<List<CoinDto>>
 
     @GET(
-        "coins/ethereum?localization=false&tickers=false&community_data=false&developer_data=false"
+        "coins/ethereum?localization=false&tickers=false&community_data=false&developer_data=false&sparkline=true"
     )
     suspend fun getCoinDetail(): Response<CoinDetailDto>
 }
