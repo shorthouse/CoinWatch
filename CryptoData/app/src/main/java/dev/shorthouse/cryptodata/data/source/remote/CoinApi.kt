83fb1e08a130b21bff2b6f96b168b767f182a159
Harry S (Macbook) <shorthouse20@gmail.com>
2023-06-24 14:57:54 +0100

    Refactor retrofitAPI to switch from hardcoded paths and queries to parameters

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/data/source/remote/CoinApi.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/data/source/remote/CoinApi.kt
@@ -4,15 +4,29 @@ import dev.shorthouse.cryptodata.data.source.remote.dto.CoinDetailDto
 import dev.shorthouse.cryptodata.data.source.remote.dto.CoinDto
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface CoinApi {
-    @GET(
-        "coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h&locale=en&precision=2"
-    )
-    suspend fun getCryptocurrencies(): Response<List<CoinDto>>
+    @GET("coins/markets")
+    suspend fun getCoins(
+        @Query("vs_currency") currency: String = "usd",
+        @Query("price_change_percentage") priceChangePercentage: String = "24h",
+        @Query("precision") currentPricePrecision: String = "2",
+        @Query("order") order: String = "market_cap_desc",
+        @Query("per_page") coinsPerPage: Int = 100,
+        @Query("page") pageNum: Int = 1,
+        @Query("sparkline") sparkline: Boolean = false,
+        @Query("locale") locale: String = "en"
+    ): Response<List<CoinDto>>
 
-    @GET(
-        "coins/ethereum?localization=false&tickers=false&community_data=false&developer_data=false&sparkline=true"
-    )
-    suspend fun getCoinDetail(): Response<CoinDetailDto>
+    @GET("coins/{coinId}")
+    suspend fun getCoinDetail(
+        @Path("coinId") coinId: String,
+        @Query("localization") localization: Boolean = false,
+        @Query("tickers") tickets: Boolean = false,
+        @Query("community_data") communityData: Boolean = false,
+        @Query("developer_data") developerData: Boolean = false,
+        @Query("sparkline") sparkline: Boolean = true
+    ): Response<CoinDetailDto>
 }
