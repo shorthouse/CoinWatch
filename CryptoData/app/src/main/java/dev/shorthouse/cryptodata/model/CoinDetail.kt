252a8a88da9a878a9ad4a14074e711f426cb9d57
Harry S <shorthouse20@gmail.com>
2023-06-25 12:20:50 +0100

    Add date and currency formatting to coinDetail mapper

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/model/CoinDetail.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/model/CoinDetail.kt
@@ -1,9 +1,12 @@
 package dev.shorthouse.cryptodata.model
 
 import dev.shorthouse.cryptodata.data.source.remote.dto.CoinDetailApiModel
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 import java.text.NumberFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.Currency
 import java.util.Locale
 
 data class CoinDetail(
@@ -16,47 +19,51 @@ data class CoinDetail(
     val historicalPrices7d: List<Double>,
     val marketCapRank: Int,
     val marketCap: String,
-    val totalSupply: Double,
+    val circulatingSupply: String,
     val allTimeLow: String,
     val allTimeHigh: String,
     val allTimeLowDate: String,
     val allTimeHighDate: String,
-    val genesisDate: String
 )
 
 fun CoinDetailApiModel.toCoinDetail(): CoinDetail {
-    val dateFormatter = DateTimeFormatter.ofPattern("d MM yyyy", Locale.getDefault())
-//
-    val allTimeLowLocalDate = LocalDateTime.parse(marketData.allTimeLowDate.usd)
-//    val allTimeHighLocalDate = LocalDateTime.parse(marketData.allTimeHighDate.usd)
-//    val genesisLocalDate = LocalDateTime.parse(
-//        genesisDate,
-//        DateTimeFormatter.ofPattern("yyyy-mm-dd")
-//    )
+    val currencyDecimalFormatter = NumberFormat.getCurrencyInstance(Locale.getDefault()).apply {
+        currency = Currency.getInstance("USD")
+    }
+    val currencyIntegerFormatter = NumberFormat.getCurrencyInstance(Locale.getDefault()).apply {
+        currency = Currency.getInstance("USD")
+        maximumFractionDigits = 0
+    }
 
-    val currencyFormatter = NumberFormat.getCurrencyInstance(Locale.US)
+    val dateFormatter = DateTimeFormatter.ofPattern("d MMM yyyy", Locale.getDefault())
+    val allTimeLowLocalDate = LocalDateTime.parse(
+        marketData.allTimeLowDate.usd,
+        DateTimeFormatter.ISO_DATE_TIME
+    )
+    val allTimeHighLocalDate = LocalDateTime.parse(
+        marketData.allTimeHighDate.usd,
+        DateTimeFormatter.ISO_DATE_TIME
+    )
+
+    val decimalFormat = DecimalFormat(
+        "#,###",
+        DecimalFormatSymbols.getInstance(Locale.getDefault())
+    )
 
     return CoinDetail(
         id = id,
         name = name,
         symbol = symbol.uppercase(),
         image = image.large,
-        currentPrice = currencyFormatter.format(marketData.currentPrice.usd),
+        currentPrice = currencyDecimalFormatter.format(marketData.currentPrice.usd),
         priceChangePercentage24h = marketData.priceChangePercentage24h,
         historicalPrices7d = marketData.historicalPrices7d.usd,
         marketCapRank = marketData.marketCapRank,
-        marketCap = currencyFormatter.format(marketData.marketCap.usd),
-        totalSupply = marketData.totalSupply,
-        allTimeLow = currencyFormatter.format(marketData.allTimeLow.usd),
-        allTimeHigh = currencyFormatter.format(marketData.allTimeHigh.usd),
-        allTimeLowDate = "formatDate(marketData.allTimeLowDate.usd)",
-        allTimeHighDate = "allTimeHighLocalDate.format(dateFormatter)",
-        genesisDate = "genesisLocalDate.format(dateFormatter)"
+        marketCap = currencyIntegerFormatter.format(marketData.marketCap.usd),
+        circulatingSupply = decimalFormat.format(marketData.circulatingSupply),
+        allTimeLow = currencyDecimalFormatter.format(marketData.allTimeLow.usd),
+        allTimeHigh = currencyDecimalFormatter.format(marketData.allTimeHigh.usd),
+        allTimeLowDate = dateFormatter.format(allTimeLowLocalDate),
+        allTimeHighDate = dateFormatter.format(allTimeHighLocalDate),
     )
-}
-
-private fun formatDate(dateTimeString: String): String {
-    val formatter = DateTimeFormatter.ofPattern("d MMM yyyy", Locale.getDefault())
-    val dateTime = LocalDateTime.parse(dateTimeString, DateTimeFormatter.ISO_DATE_TIME)
-    return dateTime.format(formatter)
 }
