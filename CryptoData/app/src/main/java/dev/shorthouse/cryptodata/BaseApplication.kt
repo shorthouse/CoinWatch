c17524eee492c8c2da71474b6aaaae5d2bbde137
Harry S (Macbook) <shorthouse20@gmail.com>
2023-07-08 15:00:12 +0100

    Plant timber tree

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/BaseApplication.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/BaseApplication.kt
@@ -2,6 +2,16 @@ package dev.shorthouse.cryptodata
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class BaseApplication : Application()
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+            Timber.d("Timber DebugTree planted")
+        }
+    }
+}
