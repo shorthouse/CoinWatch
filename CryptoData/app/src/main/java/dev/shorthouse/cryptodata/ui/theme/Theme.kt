71397a1e73bd766e98822697ac32358df20abc2e
Harry S <shorthouse20@gmail.com>
2023-07-08 12:28:25 +0100

    Set to only dark theme

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/theme/Theme.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/theme/Theme.kt
@@ -1,22 +1,17 @@
 package dev.shorthouse.cryptodata.ui.theme
 
 import android.app.Activity
+import android.provider.CalendarContract
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-    onSurfaceVariant = light_onSurfaceVariant
-)
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple200,
@@ -33,22 +28,17 @@ fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = DarkColorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = DarkColorScheme,
         typography = AppTypography,
         shapes = AppShapes,
         content = content
