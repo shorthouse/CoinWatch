16b875d8518edb0671e52a881ee5efdc5407070d
Harry S (Macbook) <shorthouse20@gmail.com>
2023-07-08 15:00:24 +0100

    Add timber error logging

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/data/repository/chart/CoinChartRepositoryImpl.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/data/repository/chart/CoinChartRepositoryImpl.kt
@@ -7,11 +7,12 @@ import dev.shorthouse.cryptodata.di.IoDispatcher
 import dev.shorthouse.cryptodata.model.CoinChart
 import dev.shorthouse.cryptodata.model.Percentage
 import dev.shorthouse.cryptodata.model.Price
+import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import javax.inject.Inject
+import timber.log.Timber
 
 class CoinChartRepositoryImpl @Inject constructor(
     private val coinNetworkDataSource: CoinNetworkDataSource,
@@ -33,9 +34,11 @@ class CoinChartRepositoryImpl @Inject constructor(
                 if (response.isSuccessful && body != null) {
                     Result.Success(body.toCoinChart())
                 } else {
+                    Timber.e("getCoinChart unsuccessful retrofit response ${response.message()}")
                     Result.Error(message = response.message())
                 }
             } catch (e: Throwable) {
+                Timber.e("getCoinChart error $e")
                 Result.Error(message = e.message)
             }
         )
