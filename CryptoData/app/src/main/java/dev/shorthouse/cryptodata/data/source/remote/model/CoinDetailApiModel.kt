bc50c8d04543305965187c3bf2106a35e44d7b00
Harry S <shorthouse20@gmail.com>
2023-07-08 12:35:12 +0100

    Make coin detail values nullable

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/data/source/remote/model/CoinDetailApiModel.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/data/source/remote/model/CoinDetailApiModel.kt
@@ -4,23 +4,23 @@ import com.google.gson.annotations.SerializedName
 
 data class CoinDetailApiModel(
     val id: String,
-    val name: String,
-    val symbol: String,
-    val image: String,
+    val name: String?,
+    val symbol: String?,
+    val image: String?,
     @SerializedName("current_price")
-    val currentPrice: Double,
+    val currentPrice: Double?,
     @SerializedName("market_cap")
-    val marketCap: Double,
+    val marketCap: Double?,
     @SerializedName("market_cap_rank")
-    val marketCapRank: Int,
+    val marketCapRank: Int?,
     @SerializedName("circulating_supply")
-    val circulatingSupply: Double,
+    val circulatingSupply: Double?,
     @SerializedName("atl")
-    val allTimeLow: Double,
+    val allTimeLow: Double?,
     @SerializedName("atl_date")
-    val allTimeLowDate: String,
+    val allTimeLowDate: String?,
     @SerializedName("ath")
-    val allTimeHigh: Double,
+    val allTimeHigh: Double?,
     @SerializedName("ath_date")
-    val allTimeHighDate: String
+    val allTimeHighDate: String?
 )
