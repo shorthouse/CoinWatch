30e765375744f03b5462a06e90f70e2ddf3cf302
Harry S (Macbook) <shorthouse20@gmail.com>
2023-07-09 15:59:25 +0100

    Use new zero white color

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/component/PercentageChange.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/component/PercentageChange.kt
@@ -4,7 +4,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import dev.shorthouse.cryptodata.model.Percentage
