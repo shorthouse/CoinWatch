9ac86fc650705135d7a279b7c1dc1bf233dcc95c
Harry S (Macbook) <shorthouse20@gmail.com>
2023-07-10 20:32:49 +0100

    Refactor percentage to use bigdecimal

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/model/Percentage.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/model/Percentage.kt
@@ -1,23 +1,25 @@
 package dev.shorthouse.cryptodata.model
 
+import java.math.BigDecimal
 import java.text.NumberFormat
 import java.util.Locale
 
-data class Percentage(val amount: Double) {
+data class Percentage(private val amount: BigDecimal) {
     companion object {
-        private val percentageFormat: NumberFormat = NumberFormat.getPercentInstance(Locale.US).apply {
-            isGroupingUsed = true
-            minimumFractionDigits = 2
-            maximumFractionDigits = 2
-        }
+        private val percentageFormat: NumberFormat =
+            NumberFormat.getPercentInstance(Locale.US).apply {
+                isGroupingUsed = true
+                minimumFractionDigits = 2
+                maximumFractionDigits = 2
+            }
     }
 
-    val isPositive: Boolean = amount > 0
-    val isNegative: Boolean = amount < 0
+    val isPositive: Boolean = amount.signum() > 0
+    val isNegative: Boolean = amount.signum() < 0
 
-    val formattedAmount: String = if (amount >= 0) {
-        "+" + percentageFormat.format(amount / 100)
+    val formattedAmount: String = if (amount.signum() >= 0) {
+        "+" + percentageFormat.format(amount.scaleByPowerOfTen(-2))
     } else {
-        percentageFormat.format(amount / 100)
+        percentageFormat.format(amount.scaleByPowerOfTen(-2))
     }
 }
