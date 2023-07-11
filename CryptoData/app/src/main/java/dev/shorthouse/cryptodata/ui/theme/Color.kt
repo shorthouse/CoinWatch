066b02b7d03de2603ede569799abcced34bf15a1
Harry S (Macbook) <shorthouse20@gmail.com>
2023-07-11 20:06:47 +0100

    Add light theme colours

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/theme/Color.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/theme/Color.kt
@@ -2,11 +2,15 @@ package dev.shorthouse.cryptodata.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val dark_onSurfaceVariant = Color(0xFFBDBDBD)
-
 val PositiveGreen = Color(0xFF12D18E)
 val NegativeRed = Color(0xFFF75555)
 val ZeroWhite = Color(0xFFFFFFFF)
+
+val light_background = Color(0xFFF7F7F7)
+val light_onBackground = Color(0xFF000000)
+val light_surface = Color(0xFFFFFFFF)
+val light_onSurface = Color(0xFF000000)
+val light_onSurfaceVariant = Color(0xFF7B848B)
 
 val Purple200 = Color(0xFFBB86FC)
 val Teal200 = Color(0xFF03DAC5)
@@ -14,3 +18,4 @@ val dark_background = Color(0xFF0C1B3A)
 val dark_onBackground = Color(0xFFFFFFFF)
 val dark_surface = Color(0xFF162544)
 val dark_onSurface = Color(0xFFFFFFFF)
+val dark_onSurfaceVariant = Color(0xFFBDBDBD)
