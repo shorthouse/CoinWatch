512b1d0ed589f8c9b02f6d2f2658ce4cfbc6df01
Harry S (Macbook) <shorthouse20@gmail.com>
2023-07-12 21:53:47 +0100

    Further consolidate typography

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/theme/Type.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/theme/Type.kt
@@ -7,20 +7,15 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 val AppTypography = Typography(
-    headlineSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Bold,
-        fontSize = 26.sp
-    ),
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Bold,
-        fontSize = 22.sp
+        fontSize = 30.sp
     ),
     titleMedium = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Bold,
-        fontSize = 18.sp
+        fontSize = 24.sp
     ),
     titleSmall = TextStyle(
         fontFamily = FontFamily.Default,
@@ -29,8 +24,8 @@ val AppTypography = Typography(
     ),
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 14.sp
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
     ),
     bodyMedium = TextStyle(
         fontFamily = FontFamily.Default,
@@ -39,12 +34,12 @@ val AppTypography = Typography(
     ),
     bodySmall = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Bold,
         fontSize = 14.sp
     ),
     labelLarge = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 16.sp
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
 )
