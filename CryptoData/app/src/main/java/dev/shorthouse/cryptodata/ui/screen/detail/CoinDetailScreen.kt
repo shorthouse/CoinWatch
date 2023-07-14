7c9340c19e0775f83ba481c4d549a4d1b8c7fe5a
Harry S <shorthouse20@gmail.com>
2023-07-14 18:00:18 +0100

    Padding tweaking to details

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/screen/detail/CoinDetailScreen.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/screen/detail/CoinDetailScreen.kt
@@ -43,15 +43,12 @@ import dev.shorthouse.cryptodata.R
 import dev.shorthouse.cryptodata.model.ChartPeriod
 import dev.shorthouse.cryptodata.model.CoinChart
 import dev.shorthouse.cryptodata.model.CoinDetail
-import dev.shorthouse.cryptodata.model.Percentage
-import dev.shorthouse.cryptodata.model.Price
 import dev.shorthouse.cryptodata.ui.component.LoadingIndicator
 import dev.shorthouse.cryptodata.ui.component.PercentageChangeChip
 import dev.shorthouse.cryptodata.ui.component.PriceGraph
 import dev.shorthouse.cryptodata.ui.previewdata.CoinDetailUiStatePreviewProvider
 import dev.shorthouse.cryptodata.ui.screen.detail.component.ChartPeriodSelector
 import dev.shorthouse.cryptodata.ui.theme.AppTheme
-import java.math.BigDecimal
 
 @Composable
 fun CoinDetailScreen(
@@ -160,43 +157,18 @@ private fun CoinDetailContent(
     modifier: Modifier = Modifier
 ) {
     Column(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
         modifier = modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
-            .padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
+            .padding(start = 16.dp, top = 4.dp, end = 16.dp, bottom = 16.dp)
     ) {
-//        Surface(shape = MaterialTheme.shapes.medium) {
-//            Row(
-//                verticalAlignment = Alignment.CenterVertically,
-//                modifier = Modifier.padding(16.dp)
-//            ) {
-//                Column {
-//                    Text(
-//                        text = coinDetail.name,
-//                        style = MaterialTheme.typography.titleMedium,
-//                    )
-//                    Text(
-//                        text = coinDetail.symbol,
-//                        style = MaterialTheme.typography.bodyLarge,
-//                        color = MaterialTheme.colorScheme.onSurfaceVariant,
-//                    )
-//                }
-//                Spacer(Modifier.weight(1f))
-//                AsyncImage(
-//                    model = coinDetail.image,
-//                    contentDescription = null,
-//                    modifier = Modifier.size(54.dp)
-//                )
-//            }
-//        }
-
         Surface(shape = MaterialTheme.shapes.medium) {
             Column(
                 verticalArrangement = Arrangement.spacedBy(16.dp),
-                modifier = Modifier.padding(vertical = 16.dp)
+                modifier = Modifier.padding(vertical = 12.dp)
             ) {
-                Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+                Column(modifier = Modifier.padding(horizontal = 12.dp)) {
                     Text(
                         text = coinDetail.currentPrice.formattedAmount,
                         style = MaterialTheme.typography.titleLarge
@@ -228,7 +200,7 @@ private fun CoinDetailContent(
                 ChartPeriodSelector(
                     selectedChartPeriod = chartPeriod,
                     onChartPeriodSelected = onClickChartPeriod,
-                    modifier = Modifier.padding(horizontal = 16.dp)
+                    modifier = Modifier.padding(horizontal = 12.dp)
                 )
             }
         }
@@ -316,7 +288,7 @@ private fun CoinDetailContent(
         }
 
         Surface(shape = MaterialTheme.shapes.medium) {
-            Column(modifier = Modifier.padding(16.dp)) {
+            Column(modifier = Modifier.padding(12.dp)) {
                 Text(
                     text = stringResource(R.string.card_header_details),
                     style = MaterialTheme.typography.titleMedium
@@ -324,7 +296,7 @@ private fun CoinDetailContent(
 
                 Spacer(Modifier.height(16.dp))
 
-                Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
                     coinDetailItems.forEachIndexed { coinDetailIndex, coinDetailListItem ->
                         if (coinDetailIndex != 0) {
                             Divider()
@@ -363,33 +335,7 @@ private fun DetailScreenPreview(
 ) {
     AppTheme {
         CoinDetailScreen(
-            uiState = CoinDetailUiState.Success(
-                CoinDetail(
-                    id = "ethereum",
-                    name = "Ethereum",
-                    symbol = "ETH",
-                    image = "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880",
-                    currentPrice = Price(BigDecimal("1879.14")),
-                    marketCapRank = "2",
-                    marketCap = Price(BigDecimal("225722901094")),
-                    circulatingSupply = "120,186,525",
-                    allTimeLow = Price(BigDecimal("0.43")),
-                    allTimeHigh = Price(BigDecimal("4878.26")),
-                    allTimeLowDate = "20 Oct 2015",
-                    allTimeHighDate = "10 Nov 2021"
-                ),
-                CoinChart(
-                    prices = listOf(
-                        BigDecimal("1755.19"), BigDecimal("1749.71"), BigDecimal("1750.94"), BigDecimal("1748.44"), BigDecimal("1743.98"), BigDecimal("1740.25"), BigDecimal("1737.53"), BigDecimal("1730.56"), BigDecimal("1738.12"), BigDecimal("1736.10"), BigDecimal("1740.20"), BigDecimal("1740.64"), BigDecimal("1741.49"), BigDecimal("1738.87"), BigDecimal("1734.92"), BigDecimal("1736.79"), BigDecimal("1743.53"), BigDecimal("1743.21"), BigDecimal("1744.75"), BigDecimal("1744.85"), BigDecimal("1741.76"), BigDecimal("1741.46"), BigDecimal("1739.82"), BigDecimal("1740.15"), BigDecimal("1745.08"), BigDecimal("1743.29"), BigDecimal("1746.12"), BigDecimal("1745.99"), BigDecimal("1744.89"), BigDecimal("1741.10"), BigDecimal("1741.91"), BigDecimal("1738.47"), BigDecimal("1737.67"), BigDecimal("1741.82"), BigDecimal("1735.95"), BigDecimal("1728.11"), BigDecimal("1657.23"), BigDecimal("1649.89"), BigDecimal("1649.71"), BigDecimal("1650.68"), BigDecimal("1654.04"), BigDecimal("1648.55"), BigDecimal("1650.10"), BigDecimal("1651.87"), BigDecimal("1651.29"), BigDecimal("1642.75"), BigDecimal("1637.79"), BigDecimal("1635.80"), BigDecimal("1637.01"), BigDecimal("1632.46"), BigDecimal("1633.31"), BigDecimal("1640.08"), BigDecimal("1638.61"), BigDecimal("1645.47"), BigDecimal("1643.50"), BigDecimal("1640.57"), BigDecimal("1640.41"), BigDecimal("1641.38"), BigDecimal("1660.21"), BigDecimal("1665.73"), BigDecimal("1660.33"), BigDecimal("1665.65"), BigDecimal("1664.11"), BigDecimal("1665.71"), BigDecimal("1661.90"), BigDecimal("1661.17"), BigDecimal("1662.54"), BigDecimal("1665.58"), BigDecimal("1666.27"), BigDecimal("1669.82"), BigDecimal("1671.34"), BigDecimal("1669.87"), BigDecimal("1670.62"), BigDecimal("1668.97"), BigDecimal("1668.86"), BigDecimal("1664.58"), BigDecimal("1665.96"), BigDecimal("1664.53"), BigDecimal("1656.15"), BigDecimal("1670.91"), BigDecimal("1685.59"), BigDecimal("1693.69"), BigDecimal("1718.10"), BigDecimal("1719.56"), BigDecimal("1724.42"), BigDecimal("1717.22"), BigDecimal("1718.34"), BigDecimal("1716.38"), BigDecimal("1715.37"), BigDecimal("1716.46"), BigDecimal("1719.39"), BigDecimal("1717.94"), BigDecimal("1722.92"), BigDecimal("1755.97"), BigDecimal("1749.11"), BigDecimal("1742.58"), BigDecimal("1742.88"), BigDecimal("1743.36"), BigDecimal("1742.95"), BigDecimal("1739.68"), BigDecimal("1736.65"), BigDecimal("1739.88"), BigDecimal("1734.35"), BigDecimal("1727.31"), BigDecimal("1728.35"), BigDecimal("1724.05"), BigDecimal("1730.04"), BigDecimal("1726.87"), BigDecimal("1727.71"), BigDecimal("1728.49"), BigDecimal("1729.93"), BigDecimal("1726.37"), BigDecimal("1722.92"), BigDecimal("1726.67"), BigDecimal("1724.76"), BigDecimal("1728.41"), BigDecimal("1729.20"), BigDecimal("1728.20"), BigDecimal("1727.98"), BigDecimal("1729.96"), BigDecimal("1727.80"), BigDecimal("1732.04"), BigDecimal("1730.22"), BigDecimal("1733.16"), BigDecimal("1734.14"), BigDecimal("1734.31"), BigDecimal("1739.62"), BigDecimal("1737.76"), BigDecimal("1739.52"), BigDecimal("1742.98"), BigDecimal("1738.36") // ktlint-disable argument-list-wrapping
-                    ),
-                    minPrice = Price(BigDecimal("1632.46")),
-                    minPriceChangePercentage = Percentage(BigDecimal("15.11")),
-                    maxPrice = Price(BigDecimal("1922.83")),
-                    maxPriceChangePercentage = Percentage(BigDecimal("-2.27")),
-                    periodPriceChangePercentage = Percentage(BigDecimal("7.06")),
-                ),
-                chartPeriod = ChartPeriod.Week
-            ),
+            uiState = uiState,
             onNavigateUp = {},
             onClickChartPeriod = {}
         )
