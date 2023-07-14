bf4610160307e883e9e17f9e6ff1cee582b28d2d
Harry S <shorthouse20@gmail.com>
2023-07-14 17:42:19 +0100

    Use ChartPeriod enum class

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/screen/detail/CoinDetailUiState.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/screen/detail/CoinDetailUiState.kt
@@ -1,15 +1,15 @@
 package dev.shorthouse.cryptodata.ui.screen.detail
 
+import dev.shorthouse.cryptodata.model.ChartPeriod
 import dev.shorthouse.cryptodata.model.CoinChart
 import dev.shorthouse.cryptodata.model.CoinDetail
-import kotlin.time.Duration
 
 sealed interface CoinDetailUiState {
     object Loading : CoinDetailUiState
     data class Success(
         val coinDetail: CoinDetail,
         val coinChart: CoinChart,
-        val chartPeriod: Duration
+        val chartPeriod: ChartPeriod
     ) : CoinDetailUiState
     data class Error(val message: String?) : CoinDetailUiState
 }
--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/screen/detail/CoinDetailViewModel.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/screen/detail/CoinDetailViewModel.kt
@@ -8,9 +8,7 @@ import dev.shorthouse.cryptodata.common.Constants.PARAM_COIN_ID
 import dev.shorthouse.cryptodata.common.Result
 import dev.shorthouse.cryptodata.domain.GetCoinChartUseCase
 import dev.shorthouse.cryptodata.domain.GetCoinDetailUseCase
-import javax.inject.Inject
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.days
+import dev.shorthouse.cryptodata.model.ChartPeriod
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
@@ -18,6 +16,7 @@ import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.update
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltViewModel
 class CoinDetailViewModel @Inject constructor(
@@ -28,7 +27,7 @@ class CoinDetailViewModel @Inject constructor(
     private val _uiState = MutableStateFlow<CoinDetailUiState>(CoinDetailUiState.Loading)
     val uiState = _uiState.asStateFlow()
 
-    private val chartPeriodFlow = MutableStateFlow(7.days)
+    private val chartPeriodFlow = MutableStateFlow(ChartPeriod.Day)
 
     init {
         savedStateHandle.get<String>(PARAM_COIN_ID)?.let { coinId ->
@@ -36,16 +35,16 @@ class CoinDetailViewModel @Inject constructor(
         }
     }
 
-    fun updateChartPeriod(chartPeriod: Duration) {
+    fun updateChartPeriod(chartPeriod: ChartPeriod) {
         chartPeriodFlow.value = chartPeriod
     }
 
     private fun getCoinDetail(coinId: String) {
         val coinDetailFlow = getCoinDetailUseCase(coinId = coinId)
-        val coinChartFlow = chartPeriodFlow.flatMapLatest {
+        val coinChartFlow = chartPeriodFlow.flatMapLatest { chartPeriod ->
             getCoinChartUseCase(
                 coinId = coinId,
-                chartPeriodDays = it.inWholeDays.toString()
+                chartPeriodDays = chartPeriod.stringName
             )
         }
 
