04ae079f85f206dcd46c1767a35aae9fd3b01f96
Harry S <shorthouse20@gmail.com>
2023-07-14 17:42:07 +0100

    Further beautify CoinDetail UI

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/screen/detail/CoinDetailScreen.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/screen/detail/CoinDetailScreen.kt
@@ -1,5 +1,6 @@
 package dev.shorthouse.cryptodata.ui.screen.detail
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,29 +10,28 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -40,16 +40,18 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import dev.shorthouse.cryptodata.R
+import dev.shorthouse.cryptodata.model.ChartPeriod
 import dev.shorthouse.cryptodata.model.CoinChart
 import dev.shorthouse.cryptodata.model.CoinDetail
+import dev.shorthouse.cryptodata.model.Percentage
+import dev.shorthouse.cryptodata.model.Price
 import dev.shorthouse.cryptodata.ui.component.LoadingIndicator
 import dev.shorthouse.cryptodata.ui.component.PercentageChangeChip
 import dev.shorthouse.cryptodata.ui.component.PriceGraph
 import dev.shorthouse.cryptodata.ui.previewdata.CoinDetailUiStatePreviewProvider
-import dev.shorthouse.cryptodata.ui.screen.detail.component.CoinDetailListItem
+import dev.shorthouse.cryptodata.ui.screen.detail.component.ChartPeriodSelector
 import dev.shorthouse.cryptodata.ui.theme.AppTheme
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.days
+import java.math.BigDecimal
 
 @Composable
 fun CoinDetailScreen(
@@ -70,16 +72,21 @@ fun CoinDetailScreen(
 fun CoinDetailScreen(
     uiState: CoinDetailUiState,
     onNavigateUp: () -> Unit,
-    onClickChartPeriod: (Duration) -> Unit,
+    onClickChartPeriod: (ChartPeriod) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    val isTopBarCollapsed by remember {
+        derivedStateOf { scrollBehavior.state.collapsedFraction > 0.8f }
+    }
+
     when (uiState) {
         is CoinDetailUiState.Loading -> LoadingIndicator()
         is CoinDetailUiState.Error -> Text("error")
         is CoinDetailUiState.Success -> {
             Scaffold(
                 topBar = {
-                    TopAppBar(
+                    LargeTopAppBar(
                         navigationIcon = {
                             IconButton(onClick = onNavigateUp) {
                                 Icon(
@@ -89,7 +96,41 @@ fun CoinDetailScreen(
                                 )
                             }
                         },
-                        title = {},
+                        title = {
+                            Surface(
+                                shape = MaterialTheme.shapes.medium,
+                                color = if (isTopBarCollapsed) {
+                                    MaterialTheme.colorScheme.background
+                                } else {
+                                    MaterialTheme.colorScheme.surface
+                                },
+                                modifier = Modifier.padding(end = 16.dp)
+                            ) {
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    modifier = Modifier.padding(12.dp)
+                                ) {
+                                    Column {
+                                        Text(
+                                            text = uiState.coinDetail.name,
+                                            style = MaterialTheme.typography.titleMedium,
+                                        )
+                                        Text(
+                                            text = uiState.coinDetail.symbol,
+                                            style = MaterialTheme.typography.bodyLarge,
+                                            color = MaterialTheme.colorScheme.onSurfaceVariant,
+                                        )
+                                    }
+                                    Spacer(Modifier.weight(1f))
+                                    AsyncImage(
+                                        model = uiState.coinDetail.image,
+                                        contentDescription = null,
+                                        modifier = Modifier.size(54.dp)
+                                    )
+                                }
+                            }
+                        },
+                        scrollBehavior = scrollBehavior,
                         colors = TopAppBarDefaults.topAppBarColors(
                             containerColor = MaterialTheme.colorScheme.background
                         )
@@ -104,7 +145,7 @@ fun CoinDetailScreen(
                         modifier = Modifier.padding(scaffoldPadding)
                     )
                 },
-                modifier = modifier
+                modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
             )
         }
     }
@@ -114,41 +155,41 @@ fun CoinDetailScreen(
 private fun CoinDetailContent(
     coinDetail: CoinDetail,
     coinChart: CoinChart,
-    chartPeriod: Duration,
-    onClickChartPeriod: (Duration) -> Unit,
+    chartPeriod: ChartPeriod,
+    onClickChartPeriod: (ChartPeriod) -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
-        verticalArrangement = Arrangement.spacedBy(12.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
-            .padding(start = 8.dp, end = 8.dp, bottom = 8.dp)
+            .padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
     ) {
-        Surface(shape = MaterialTheme.shapes.medium) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(16.dp)
-            ) {
-                Column {
-                    Text(
-                        text = coinDetail.name,
-                        style = MaterialTheme.typography.titleMedium,
-                    )
-                    Text(
-                        text = coinDetail.symbol,
-                        style = MaterialTheme.typography.bodyLarge,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant,
-                    )
-                }
-                Spacer(Modifier.weight(1f))
-                AsyncImage(
-                    model = coinDetail.image,
-                    contentDescription = null,
-                    modifier = Modifier.size(54.dp)
-                )
-            }
-        }
+//        Surface(shape = MaterialTheme.shapes.medium) {
+//            Row(
+//                verticalAlignment = Alignment.CenterVertically,
+//                modifier = Modifier.padding(16.dp)
+//            ) {
+//                Column {
+//                    Text(
+//                        text = coinDetail.name,
+//                        style = MaterialTheme.typography.titleMedium,
+//                    )
+//                    Text(
+//                        text = coinDetail.symbol,
+//                        style = MaterialTheme.typography.bodyLarge,
+//                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+//                    )
+//                }
+//                Spacer(Modifier.weight(1f))
+//                AsyncImage(
+//                    model = coinDetail.image,
+//                    contentDescription = null,
+//                    modifier = Modifier.size(54.dp)
+//                )
+//            }
+//        }
 
         Surface(shape = MaterialTheme.shapes.medium) {
             Column(
@@ -169,12 +210,13 @@ private fun CoinDetailContent(
                             percentage = coinChart.periodPriceChangePercentage
                         )
                         Text(
-                            text = "Last ${chartPeriod.inWholeDays} days",
+                            text = stringResource(chartPeriod.longNameId),
                             style = MaterialTheme.typography.bodyMedium,
                             color = MaterialTheme.colorScheme.onSurfaceVariant,
                         )
                     }
                 }
+
                 PriceGraph(
                     prices = coinChart.prices,
                     priceChangePercentage = coinChart.periodPriceChangePercentage,
@@ -183,42 +225,11 @@ private fun CoinDetailContent(
                         .height(200.dp)
                 )
 
-                val chartPeriodOptions = remember {
-                    listOf(
-                        1.days,
-                        7.days,
-                        30.days,
-                        365.days
-                    )
-                }
-
-                Row(
-                    horizontalArrangement = Arrangement.Center,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    chartPeriodOptions.forEach { chartPeriodOption ->
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            modifier = Modifier
-                                .selectable(
-                                    selected = (chartPeriodOption == chartPeriod),
-                                    onClick = { onClickChartPeriod(chartPeriodOption) },
-                                    role = Role.RadioButton
-                                )
-                        ) {
-                            RadioButton(
-                                selected = (chartPeriodOption == chartPeriod),
-                                onClick = null,
-                                modifier = Modifier.padding(8.dp)
-                            )
-
-                            Text(
-                                text = chartPeriodOption.inWholeDays.toString(),
-                                style = MaterialTheme.typography.bodyMedium
-                            )
-                        }
-                    }
-                }
+                ChartPeriodSelector(
+                    selectedChartPeriod = chartPeriod,
+                    onChartPeriodSelected = onClickChartPeriod,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
             }
         }
 
@@ -305,17 +316,19 @@ private fun CoinDetailContent(
         }
 
         Surface(shape = MaterialTheme.shapes.medium) {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                modifier = Modifier.padding(16.dp)
-            ) {
+            Column(modifier = Modifier.padding(16.dp)) {
                 Text(
                     text = stringResource(R.string.card_header_details),
                     style = MaterialTheme.typography.titleMedium
                 )
 
+                Spacer(Modifier.height(16.dp))
+
                 Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
-                    coinDetailItems.forEach { coinDetailListItem ->
+                    coinDetailItems.forEachIndexed { coinDetailIndex, coinDetailListItem ->
+                        if (coinDetailIndex != 0) {
+                            Divider()
+                        }
                         Row(
                             verticalAlignment = Alignment.CenterVertically,
                             horizontalArrangement = Arrangement.SpaceBetween,
@@ -338,16 +351,10 @@ private fun CoinDetailContent(
     }
 }
 
-//        CoinChartCard(
-//            coinDetail = coinDetail,
-//            coinChart = coinChart,
-//            chartPeriod = chartPeriod,
-//            onClickChartPeriod = onClickChartPeriod
-//        )
-//
-//        CoinDetailCard(
-//            coinDetail = coinDetail
-//        )
+data class CoinDetailListItem(
+    @StringRes val nameId: Int,
+    val value: String
+)
 
 @Composable
 @Preview(showBackground = true)
@@ -356,7 +363,33 @@ private fun DetailScreenPreview(
 ) {
     AppTheme {
         CoinDetailScreen(
-            uiState = uiState,
+            uiState = CoinDetailUiState.Success(
+                CoinDetail(
+                    id = "ethereum",
+                    name = "Ethereum",
+                    symbol = "ETH",
+                    image = "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880",
+                    currentPrice = Price(BigDecimal("1879.14")),
+                    marketCapRank = "2",
+                    marketCap = Price(BigDecimal("225722901094")),
+                    circulatingSupply = "120,186,525",
+                    allTimeLow = Price(BigDecimal("0.43")),
+                    allTimeHigh = Price(BigDecimal("4878.26")),
+                    allTimeLowDate = "20 Oct 2015",
+                    allTimeHighDate = "10 Nov 2021"
+                ),
+                CoinChart(
+                    prices = listOf(
+                        BigDecimal("1755.19"), BigDecimal("1749.71"), BigDecimal("1750.94"), BigDecimal("1748.44"), BigDecimal("1743.98"), BigDecimal("1740.25"), BigDecimal("1737.53"), BigDecimal("1730.56"), BigDecimal("1738.12"), BigDecimal("1736.10"), BigDecimal("1740.20"), BigDecimal("1740.64"), BigDecimal("1741.49"), BigDecimal("1738.87"), BigDecimal("1734.92"), BigDecimal("1736.79"), BigDecimal("1743.53"), BigDecimal("1743.21"), BigDecimal("1744.75"), BigDecimal("1744.85"), BigDecimal("1741.76"), BigDecimal("1741.46"), BigDecimal("1739.82"), BigDecimal("1740.15"), BigDecimal("1745.08"), BigDecimal("1743.29"), BigDecimal("1746.12"), BigDecimal("1745.99"), BigDecimal("1744.89"), BigDecimal("1741.10"), BigDecimal("1741.91"), BigDecimal("1738.47"), BigDecimal("1737.67"), BigDecimal("1741.82"), BigDecimal("1735.95"), BigDecimal("1728.11"), BigDecimal("1657.23"), BigDecimal("1649.89"), BigDecimal("1649.71"), BigDecimal("1650.68"), BigDecimal("1654.04"), BigDecimal("1648.55"), BigDecimal("1650.10"), BigDecimal("1651.87"), BigDecimal("1651.29"), BigDecimal("1642.75"), BigDecimal("1637.79"), BigDecimal("1635.80"), BigDecimal("1637.01"), BigDecimal("1632.46"), BigDecimal("1633.31"), BigDecimal("1640.08"), BigDecimal("1638.61"), BigDecimal("1645.47"), BigDecimal("1643.50"), BigDecimal("1640.57"), BigDecimal("1640.41"), BigDecimal("1641.38"), BigDecimal("1660.21"), BigDecimal("1665.73"), BigDecimal("1660.33"), BigDecimal("1665.65"), BigDecimal("1664.11"), BigDecimal("1665.71"), BigDecimal("1661.90"), BigDecimal("1661.17"), BigDecimal("1662.54"), BigDecimal("1665.58"), BigDecimal("1666.27"), BigDecimal("1669.82"), BigDecimal("1671.34"), BigDecimal("1669.87"), BigDecimal("1670.62"), BigDecimal("1668.97"), BigDecimal("1668.86"), BigDecimal("1664.58"), BigDecimal("1665.96"), BigDecimal("1664.53"), BigDecimal("1656.15"), BigDecimal("1670.91"), BigDecimal("1685.59"), BigDecimal("1693.69"), BigDecimal("1718.10"), BigDecimal("1719.56"), BigDecimal("1724.42"), BigDecimal("1717.22"), BigDecimal("1718.34"), BigDecimal("1716.38"), BigDecimal("1715.37"), BigDecimal("1716.46"), BigDecimal("1719.39"), BigDecimal("1717.94"), BigDecimal("1722.92"), BigDecimal("1755.97"), BigDecimal("1749.11"), BigDecimal("1742.58"), BigDecimal("1742.88"), BigDecimal("1743.36"), BigDecimal("1742.95"), BigDecimal("1739.68"), BigDecimal("1736.65"), BigDecimal("1739.88"), BigDecimal("1734.35"), BigDecimal("1727.31"), BigDecimal("1728.35"), BigDecimal("1724.05"), BigDecimal("1730.04"), BigDecimal("1726.87"), BigDecimal("1727.71"), BigDecimal("1728.49"), BigDecimal("1729.93"), BigDecimal("1726.37"), BigDecimal("1722.92"), BigDecimal("1726.67"), BigDecimal("1724.76"), BigDecimal("1728.41"), BigDecimal("1729.20"), BigDecimal("1728.20"), BigDecimal("1727.98"), BigDecimal("1729.96"), BigDecimal("1727.80"), BigDecimal("1732.04"), BigDecimal("1730.22"), BigDecimal("1733.16"), BigDecimal("1734.14"), BigDecimal("1734.31"), BigDecimal("1739.62"), BigDecimal("1737.76"), BigDecimal("1739.52"), BigDecimal("1742.98"), BigDecimal("1738.36") // ktlint-disable argument-list-wrapping
+                    ),
+                    minPrice = Price(BigDecimal("1632.46")),
+                    minPriceChangePercentage = Percentage(BigDecimal("15.11")),
+                    maxPrice = Price(BigDecimal("1922.83")),
+                    maxPriceChangePercentage = Percentage(BigDecimal("-2.27")),
+                    periodPriceChangePercentage = Percentage(BigDecimal("7.06")),
+                ),
+                chartPeriod = ChartPeriod.Week
+            ),
             onNavigateUp = {},
             onClickChartPeriod = {}
         )
