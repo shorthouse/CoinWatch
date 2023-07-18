84b6944aed7cf6571b86743a9f2661972d426f3e
Harry S <shorthouse20@gmail.com>
2023-07-18 19:44:41 +0100

    Remove light theme

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/theme/Theme.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/theme/Theme.kt
@@ -1,24 +1,11 @@
 package dev.shorthouse.cryptodata.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-
-private val LightColorScheme = lightColorScheme(
-    primary = light_primary,
-    onPrimary = light_onPrimary,
-    surface = light_surface,
-    onSurface = light_onSurface,
-    onSurfaceVariant = light_onSurfaceVariant,
-    background = light_background,
-    onBackground = light_onBackground
-)
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple200,
-    secondary = Teal200,
     background = dark_background,
     onBackground = dark_onBackground,
     surface = dark_surface,
@@ -27,12 +14,9 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 @Composable
-fun AppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
+fun AppTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme,
+        colorScheme = DarkColorScheme,
         typography = AppTypography,
         shapes = AppShapes,
         content = content
