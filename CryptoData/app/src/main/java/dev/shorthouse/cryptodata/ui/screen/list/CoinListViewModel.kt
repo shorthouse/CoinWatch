47da66aa301d3fa9f743c777c19f6092b10fb037
Harry S <shorthouse20@gmail.com>
2023-07-19 20:34:09 +0100

    Add and consume Market Stats

--- CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/screen/list/CoinListViewModel.kt
+++ CryptoData/app/src/main/java/dev/shorthouse/cryptodata/ui/screen/list/CoinListViewModel.kt
@@ -5,34 +5,57 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.shorthouse.cryptodata.common.Result
 import dev.shorthouse.cryptodata.domain.GetCoinsUseCase
-import javax.inject.Inject
+import dev.shorthouse.cryptodata.domain.GetMarketStatsUseCase
+import dev.shorthouse.cryptodata.ui.model.TimeOfDay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import timber.log.Timber
+import java.time.LocalDateTime
+import javax.inject.Inject
 
 @HiltViewModel
 class CoinListViewModel @Inject constructor(
-    private val getCoinsUseCase: GetCoinsUseCase
+    private val getCoinsUseCase: GetCoinsUseCase,
+    private val getMarketStatsUseCase: GetMarketStatsUseCase
 ) : ViewModel() {
     private val _uiState = MutableStateFlow<CoinListUiState>(CoinListUiState.Loading)
     val uiState = _uiState.asStateFlow()
 
     init {
-        getCoins()
+        initialiseUiState()
     }
 
-    private fun getCoins() {
-        getCoinsUseCase().onEach { result ->
-            when (result) {
-                is Result.Success -> {
-                    _uiState.update { CoinListUiState.Success(result.data) }
+    private fun initialiseUiState() {
+        val coinsFlow = getCoinsUseCase()
+        val marketStatsFlow = getMarketStatsUseCase()
+
+        combine(coinsFlow, marketStatsFlow) { coinsResult, marketStatsResult ->
+            when {
+                coinsResult is Result.Error -> {
+                    Timber.e("getCoins error ${coinsResult.message}")
+                    _uiState.update { CoinListUiState.Error(coinsResult.message) }
                 }
-                is Result.Error -> {
-                    Timber.e("getCoins error ${result.message}")
-                    _uiState.update { CoinListUiState.Error(result.message) }
+                marketStatsResult is Result.Error -> {
+                    Timber.e("getMarketStats error ${marketStatsResult.message}")
+                    _uiState.update { CoinListUiState.Error(marketStatsResult.message) }
+                }
+                coinsResult is Result.Success && marketStatsResult is Result.Success -> {
+                    val timeOfDay = when (LocalDateTime.now().hour) {
+                        in 0..11 -> TimeOfDay.Morning
+                        in 12..17 -> TimeOfDay.Afternoon
+                        else -> TimeOfDay.Evening
+                    }
+
+                    _uiState.update {
+                        CoinListUiState.Success(
+                            coins = coinsResult.data,
+                            marketStats = marketStatsResult.data,
+                            timeOfDay = timeOfDay
+                        )
+                    }
                 }
             }
         }.launchIn(viewModelScope)
