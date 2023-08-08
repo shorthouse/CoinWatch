1d848cc6bb9f370b5032126c440ae283c389d4af
Harry S (Macbook) <shorthouse20@gmail.com>
2023-08-08 22:08:36 +0100

    Switch API CoinGecko -> CoinRanking

--- CoinWatch/app/src/main/java/dev/shorthouse/coinwatch/ui/screen/list/component/CoinListItem.kt
+++ CoinWatch/app/src/main/java/dev/shorthouse/coinwatch/ui/screen/list/component/CoinListItem.kt
@@ -1,5 +1,6 @@
 package dev.shorthouse.coinwatch.ui.screen.list.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,17 +9,28 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter.State.Empty.painter
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.SvgDecoder
+import coil.request.ImageRequest
+import dev.shorthouse.coinwatch.R
 import dev.shorthouse.coinwatch.model.Coin
 import dev.shorthouse.coinwatch.ui.component.PercentageChange
 import dev.shorthouse.coinwatch.ui.previewdata.CoinPreviewProvider
@@ -29,23 +41,25 @@ fun CoinListItem(
     coin: Coin,
     onCoinClick: (Coin) -> Unit,
     cardShape: Shape,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Surface(
         shape = cardShape,
         modifier = modifier
             .fillMaxWidth()
-            .clickable { onCoinClick(coin) }
+            .clickable { onCoinClick(coin) },
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(vertical = 12.dp, horizontal = 16.dp)
+            modifier = Modifier.padding(vertical = 12.dp, horizontal = 16.dp),
         ) {
             AsyncImage(
-                model = coin.image,
+                model = ImageRequest.Builder(LocalContext.current)
+                    .decoderFactory(SvgDecoder.Factory())
+                    .data(coin.image)
+                    .build(),
+                modifier = Modifier.size(32.dp),
                 contentDescription = null,
-                alignment = Alignment.Center,
-                modifier = Modifier.size(32.dp)
             )
 
             Spacer(Modifier.width(16.dp))
@@ -53,20 +67,20 @@ fun CoinListItem(
             Column(modifier = Modifier.weight(1f)) {
                 Text(
                     text = coin.name,
-                    style = MaterialTheme.typography.bodyMedium
+                    style = MaterialTheme.typography.bodyMedium,
                 )
 
                 Text(
                     text = coin.symbol,
                     style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
                 )
             }
 
             Column(horizontalAlignment = Alignment.End) {
                 Text(
                     coin.currentPrice.formattedAmount,
-                    style = MaterialTheme.typography.bodyMedium
+                    style = MaterialTheme.typography.bodyMedium,
                 )
 
                 PercentageChange(percentage = coin.priceChangePercentage24h)
@@ -78,13 +92,13 @@ fun CoinListItem(
 @Composable
 @Preview
 private fun CoinListItemPreview(
-    @PreviewParameter(CoinPreviewProvider::class) coin: Coin
+    @PreviewParameter(CoinPreviewProvider::class) coin: Coin,
 ) {
     AppTheme {
         CoinListItem(
             coin = coin,
             onCoinClick = {},
-            cardShape = MaterialTheme.shapes.medium
+            cardShape = MaterialTheme.shapes.medium,
         )
     }
 }
