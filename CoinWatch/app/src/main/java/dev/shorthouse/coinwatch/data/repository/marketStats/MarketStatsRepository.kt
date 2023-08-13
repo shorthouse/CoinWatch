096004b7ab32d329b0db089e9fdde1290a15bf5a
Harry S (Macbook) <shorthouse20@gmail.com>
2023-08-13 18:24:54 +0100

    Remove market stats, no longer relevant with new API

--- CoinWatch/app/src/main/java/dev/shorthouse/coinwatch/data/repository/marketStats/MarketStatsRepository.kt
+++
@@ -1,9 +0,0 @@
-package dev.shorthouse.coinwatch.data.repository.marketStats
-
-import dev.shorthouse.coinwatch.common.Result
-import dev.shorthouse.coinwatch.model.MarketStats
-import kotlinx.coroutines.flow.Flow
-
-interface MarketStatsRepository {
-    fun getMarketStats(): Flow<Result<MarketStats>>
-}
