b0a92b0e68603a427cbba0ce08c67fca19c3e7b6
Harry S (Macbook) <shorthouse20@gmail.com>
2023-08-13 18:25:10 +0100

    Remove market stats, no longer relevant with new API

--- CoinWatch/app/src/main/java/dev/shorthouse/coinwatch/data/source/remote/CoinApi.kt
+++ CoinWatch/app/src/main/java/dev/shorthouse/coinwatch/data/source/remote/CoinApi.kt
@@ -3,7 +3,6 @@ package dev.shorthouse.coinwatch.data.source.remote
 import dev.shorthouse.coinwatch.data.source.remote.model.CoinChartApiModel
 import dev.shorthouse.coinwatch.data.source.remote.model.CoinDetailApiModel
 import dev.shorthouse.coinwatch.data.source.remote.model.CoinsApiModel
-import dev.shorthouse.coinwatch.data.source.remote.model.MarketStatsApiModel
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -31,7 +30,4 @@ interface CoinApi {
         @Query("referenceCurrencyUuid") currencyUUID: String = "yhjMzLPhuIDl",
         @Query("timePeriod") chartPeriod: String = "24h"
     ): Response<CoinChartApiModel>
-
-    @GET("global")
-    suspend fun getMarketStats(): Response<MarketStatsApiModel>
 }
