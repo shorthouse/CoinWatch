5cbb7d087868b3cf96cf7adf07898bd0ffae6b10
Harry S <shorthouse20@gmail.com>
2023-10-04 21:39:02 +0100

    Standardise empty states

--- app/src/main/java/dev/shorthouse/coinwatch/ui/screen/search/component/SearchEmptyState.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/ui/screen/search/component/SearchEmptyState.kt
@@ -1,7 +1,9 @@
 package dev.shorthouse.coinwatch.ui.screen.search.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -10,7 +12,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,17 +27,14 @@ import dev.shorthouse.coinwatch.ui.theme.AppTheme
 fun SearchEmptyState(
     modifier: Modifier = Modifier
 ) {
-    Surface(
-        shape = MaterialTheme.shapes.medium,
-        color = MaterialTheme.colorScheme.background,
-        modifier = modifier
-            .fillMaxWidth()
-            .fillMaxHeight(0.7f)
-    ) {
+    Box(modifier = modifier.background(MaterialTheme.colorScheme.background)) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
-            modifier = Modifier.padding(12.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.7f)
+                .padding(12.dp)
         ) {
             Image(
                 painter = painterResource(R.drawable.empty_state_search),
@@ -52,6 +50,8 @@ fun SearchEmptyState(
                 color = MaterialTheme.colorScheme.onBackground
             )
 
+            Spacer(Modifier.height(4.dp))
+
             Text(
                 text = stringResource(R.string.empty_state_search_subtitle),
                 style = MaterialTheme.typography.bodyMedium,
@@ -61,8 +61,8 @@ fun SearchEmptyState(
     }
 }
 
-@Preview
 @Composable
+@Preview
 private fun SearchEmptyStatePreview() {
     AppTheme {
         SearchEmptyState()
