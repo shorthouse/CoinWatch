76c63a2a262f0b1907151e75d4ae1c3d380f5ae4
Harry S <shorthouse20@gmail.com>
2023-10-04 21:37:36 +0100

    Extract out search results mapper

---
+++ app/src/main/java/dev/shorthouse/coinwatch/data/mapper/CoinSearchResultsMapper.kt
@@ -0,0 +1,27 @@
+package dev.shorthouse.coinwatch.data.mapper
+
+import dev.shorthouse.coinwatch.common.Mapper
+import dev.shorthouse.coinwatch.data.source.remote.model.CoinSearchResultsApiModel
+import dev.shorthouse.coinwatch.model.Price
+import dev.shorthouse.coinwatch.model.SearchCoin
+import javax.inject.Inject
+
+class CoinSearchResultsMapper @Inject constructor() :
+    Mapper<CoinSearchResultsApiModel, List<SearchCoin>> {
+    override fun mapApiModelToModel(from: CoinSearchResultsApiModel): List<SearchCoin> {
+        val validSearchResultsCoins = from.coinsSearchResultsData?.coinSearchResults
+            .orEmpty()
+            .filterNotNull()
+            .filter { it.id != null }
+
+        return validSearchResultsCoins.map { searchResultsCoin ->
+            SearchCoin(
+                id = searchResultsCoin.id!!,
+                name = searchResultsCoin.name.orEmpty(),
+                symbol = searchResultsCoin.symbol.orEmpty(),
+                imageUrl = searchResultsCoin.imageUrl.orEmpty(),
+                currentPrice = Price(searchResultsCoin.currentPrice)
+            )
+        }
+    }
+}
--- app/src/main/java/dev/shorthouse/coinwatch/data/repository/searchResults/CoinSearchResultsRepositoryImpl.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/data/repository/searchResults/CoinSearchResultsRepositoryImpl.kt
@@ -1,44 +1,34 @@
 package dev.shorthouse.coinwatch.data.repository.searchResults
 
 import dev.shorthouse.coinwatch.common.Result
+import dev.shorthouse.coinwatch.data.mapper.CoinSearchResultsMapper
 import dev.shorthouse.coinwatch.data.source.remote.CoinNetworkDataSource
-import dev.shorthouse.coinwatch.data.source.remote.model.CoinSearchResultsApiModel
 import dev.shorthouse.coinwatch.di.IoDispatcher
-import dev.shorthouse.coinwatch.model.Price
 import dev.shorthouse.coinwatch.model.SearchCoin
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import timber.log.Timber
 
 class CoinSearchResultsRepositoryImpl @Inject constructor(
     private val coinNetworkDataSource: CoinNetworkDataSource,
+    private val coinSearchResultsMapper: CoinSearchResultsMapper,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : CoinSearchResultsRepository {
     override suspend fun getCoinSearchResults(searchQuery: String): Result<List<SearchCoin>> {
-        val response = coinNetworkDataSource.getCoinSearchResults(searchQuery = searchQuery)
-        val body = response.body()
+        return withContext(ioDispatcher) {
+            val response = coinNetworkDataSource.getCoinSearchResults(searchQuery = searchQuery)
+            val body = response.body()
 
-        if (response.isSuccessful && body != null) {
-            return Result.Success(body.toSearchCoins())
-        } else {
-            Timber.e("getCoinSearchResults unsuccessful retrofit response ${response.message()}")
-            return Result.Error("Unable to fetch coin search results")
-        }
-    }
-
-    private fun CoinSearchResultsApiModel.toSearchCoins(): List<SearchCoin> {
-        return coinsSearchResultsData.coinSearchResults
-            .orEmpty()
-            .filterNotNull()
-            .filter { it.id != null }
-            .map { coinSearchResultsApiModel ->
-                SearchCoin(
-                    id = coinSearchResultsApiModel.id!!,
-                    name = coinSearchResultsApiModel.name.orEmpty(),
-                    symbol = coinSearchResultsApiModel.symbol.orEmpty(),
-                    imageUrl = coinSearchResultsApiModel.iconUrl.orEmpty(),
-                    currentPrice = Price(coinSearchResultsApiModel.currentPrice)
+            if (response.isSuccessful && body?.coinsSearchResultsData != null) {
+                val coinSearchResults = coinSearchResultsMapper.mapApiModelToModel(body)
+                Result.Success(coinSearchResults)
+            } else {
+                Timber.e(
+                    "getCoinSearchResults unsuccessful retrofit response ${response.message()}"
                 )
+                Result.Error("Unable to fetch coin search results")
             }
+        }
     }
 }
