c927c5d38f24267615908dbc01b897df46a5494c
Harry S (Macbook) <shorthouse20@gmail.com>
2023-10-05 18:30:11 +0100

    Add try catch to get coin search results

--- app/src/main/java/dev/shorthouse/coinwatch/data/repository/searchResults/CoinSearchResultsRepositoryImpl.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/data/repository/searchResults/CoinSearchResultsRepositoryImpl.kt
@@ -16,19 +16,24 @@ class CoinSearchResultsRepositoryImpl @Inject constructor(
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : CoinSearchResultsRepository {
     override suspend fun getCoinSearchResults(searchQuery: String): Result<List<SearchCoin>> {
-        return withContext(ioDispatcher) {
-            val response = coinNetworkDataSource.getCoinSearchResults(searchQuery = searchQuery)
-            val body = response.body()
+        return try {
+            withContext(ioDispatcher) {
+                val response = coinNetworkDataSource.getCoinSearchResults(searchQuery = searchQuery)
+                val body = response.body()
 
-            if (response.isSuccessful && body?.coinsSearchResultsData != null) {
-                val coinSearchResults = coinSearchResultsMapper.mapApiModelToModel(body)
-                Result.Success(coinSearchResults)
-            } else {
-                Timber.e(
-                    "getCoinSearchResults unsuccessful retrofit response ${response.message()}"
-                )
-                Result.Error("Unable to fetch coin search results")
+                if (response.isSuccessful && body?.coinsSearchResultsData != null) {
+                    val coinSearchResults = coinSearchResultsMapper.mapApiModelToModel(body)
+                    Result.Success(coinSearchResults)
+                } else {
+                    Timber.e(
+                        "getCoinSearchResults unsuccessful retrofit response ${response.message()}"
+                    )
+                    Result.Error("Unable to fetch coin search results")
+                }
             }
+        } catch (e: Exception) {
+            Timber.e("getCoinSearchResults error ${e.message}")
+            Result.Error("Unable to fetch coin search results")
         }
     }
 }
