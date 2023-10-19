8549b13bdebfd90a0733f554ba8564d525c0f5a0
Harry S <shorthouse20@gmail.com>
2023-10-19 19:55:55 +0100

    Add error state test

---
+++ app/src/androidTest/java/dev/shorthouse/coinwatch/ui/component/ErrorStateTest.kt
@@ -0,0 +1,108 @@
+package dev.shorthouse.coinwatch.ui.component
+
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.google.common.truth.Truth.assertThat
+import dev.shorthouse.coinwatch.ui.theme.AppTheme
+import org.junit.Rule
+import org.junit.Test
+
+class ErrorStateTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun when_displayingBaseErrorState_should_displayExpectedComponents() {
+        composeTestRule.setContent {
+            AppTheme {
+                ErrorState(
+                    message = null,
+                    onRetry = {}
+                )
+            }
+        }
+
+        composeTestRule.apply {
+            onNodeWithContentDescription("Error").assertIsDisplayed()
+            onNodeWithText("An error has occurred").assertIsDisplayed()
+            onNodeWithText("Retry").assertIsDisplayed().assertHasClickAction()
+        }
+    }
+
+    @Test
+    fun when_messageIsProvided_should_displayMessage() {
+        composeTestRule.setContent {
+            AppTheme {
+                ErrorState(
+                    message = "Error message",
+                    onRetry = {}
+                )
+            }
+        }
+
+        composeTestRule.apply {
+            onNodeWithText("Error message").assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun when_retryClicked_should_callOnRetry() {
+        var onRetryCalled = false
+
+        composeTestRule.setContent {
+            AppTheme {
+                ErrorState(
+                    message = null,
+                    onRetry = { onRetryCalled = true }
+                )
+            }
+        }
+
+        composeTestRule.apply {
+            onNodeWithText("Retry").performClick()
+            assertThat(onRetryCalled).isTrue()
+        }
+    }
+
+    @Test
+    fun when_navigateUpProvided_should_displayBackButton() {
+        composeTestRule.setContent {
+            AppTheme {
+                ErrorState(
+                    message = null,
+                    onRetry = {},
+                    onNavigateUp = {}
+                )
+            }
+        }
+
+        composeTestRule.apply {
+            onNodeWithContentDescription("Back").assertIsDisplayed().assertHasClickAction()
+        }
+    }
+
+    @Test
+    fun when_navigateUpClicked_should_callOnNavigateUp() {
+        var onNavigateUpCalled = false
+
+        composeTestRule.setContent {
+            AppTheme {
+                ErrorState(
+                    message = null,
+                    onRetry = {},
+                    onNavigateUp = { onNavigateUpCalled = true }
+                )
+            }
+        }
+
+        composeTestRule.apply {
+            onNodeWithContentDescription("Back").performClick()
+            assertThat(onNavigateUpCalled).isTrue()
+        }
+    }
+}
