bbc0d77c515a126c7bbeb923e901e5db8115bf16
Harry S (Macbook) <shorthouse20@gmail.com>
2023-10-21 13:14:15 +0100

    Add datastore hilt di module

---
+++ app/src/main/java/dev/shorthouse/coinwatch/di/DataStoreModule.kt
@@ -0,0 +1,32 @@
+package dev.shorthouse.coinwatch.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.dataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dev.shorthouse.coinwatch.data.datastore.UserPreferences
+import dev.shorthouse.coinwatch.data.datastore.UserPreferencesSerializer
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class DataStoreModule {
+
+    @Provides
+    @Singleton
+    fun provideProtoDataStore(@ApplicationContext appContext: Context): DataStore<UserPreferences> {
+        return DataStoreFactory.create(
+            serializer = UserPreferencesSerializer,
+            produceFile = { appContext.dataStoreFile("user_preferences.pb") },
+            corruptionHandler = ReplaceFileCorruptionHandler(
+                produceNewData = { UserPreferences() }
+            )
+        )
+    }
+}
