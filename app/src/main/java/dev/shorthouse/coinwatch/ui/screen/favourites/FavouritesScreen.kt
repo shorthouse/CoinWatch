e44f0e58e968a854f77ee974d0412b764b7c17ee
Harry S <shorthouse20@gmail.com>
2023-10-23 20:22:44 +0100

    Add scroll logic to favourites

--- app/src/main/java/dev/shorthouse/coinwatch/ui/screen/favourites/FavouritesScreen.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/ui/screen/favourites/FavouritesScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -72,7 +73,8 @@ fun FavouriteScreen(
                         onCoinClick = onCoinClick,
                         modifier = Modifier.padding(scaffoldPadding)
                     )
-                }
+                },
+                modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
             )
         }
 
