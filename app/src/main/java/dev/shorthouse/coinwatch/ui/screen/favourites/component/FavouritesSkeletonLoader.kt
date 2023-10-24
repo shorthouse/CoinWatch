1bff5967cf61070446b14d918d42092e1b4fdfc3
Harry S <shorthouse20@gmail.com>
2023-10-24 18:50:12 +0100

    Add favourites skeleton loader

---
+++ app/src/main/java/dev/shorthouse/coinwatch/ui/screen/favourites/component/FavouritesSkeletonLoader.kt
@@ -0,0 +1,84 @@
+package dev.shorthouse.coinwatch.ui.screen.favourites.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.shorthouse.coinwatch.R
+import dev.shorthouse.coinwatch.ui.component.SkeletonSurface
+import dev.shorthouse.coinwatch.ui.theme.AppTheme
+
+@Composable
+fun FavouritesSkeletonLoader(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = {
+            FavouritesSkeletonTopBar()
+        },
+        content = { scaffoldPadding ->
+            FavouritesSkeletonContent(modifier = Modifier.padding(scaffoldPadding))
+        },
+        modifier = modifier
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FavouritesSkeletonTopBar(modifier: Modifier = Modifier) {
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.favourites_screen),
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier.offset(x = (-4).dp)
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            scrolledContainerColor = MaterialTheme.colorScheme.background
+        ),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun FavouritesSkeletonContent(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 140.dp),
+        contentPadding = PaddingValues(12.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        userScrollEnabled = false,
+        modifier = modifier
+    ) {
+        repeat(20) {
+            item {
+                SkeletonSurface(
+                    modifier = Modifier.height(200.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun FavouritesSkeletonLoaderPreview() {
+    AppTheme {
+        FavouritesSkeletonLoader()
+    }
+}
