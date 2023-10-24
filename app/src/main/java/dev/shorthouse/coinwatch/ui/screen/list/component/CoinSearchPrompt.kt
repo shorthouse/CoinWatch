2525252312bdef59816b38061e9d42741aae2dcd
Harry S (Macbook) <shorthouse20@gmail.com>
2023-10-24 18:43:50 +0100

    Extract out search prompt composable

---
+++ app/src/main/java/dev/shorthouse/coinwatch/ui/screen/list/component/CoinSearchPrompt.kt
@@ -0,0 +1,46 @@
+package dev.shorthouse.coinwatch.ui.screen.list.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import dev.shorthouse.coinwatch.R
+import dev.shorthouse.coinwatch.ui.theme.AppTheme
+
+@Composable
+fun CoinSearchPrompt(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = stringResource(R.string.cant_find_coin_prompt),
+            style = MaterialTheme.typography.bodySmall,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+
+        Text(
+            text = stringResource(R.string.search_coin_prompt),
+            style = MaterialTheme.typography.bodySmall,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+    }
+}
+
+@Composable
+@Preview
+fun CoinSearchPromptPreview() {
+    AppTheme {
+        CoinSearchPrompt()
+    }
+}
