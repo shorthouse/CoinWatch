6214a4c021d170cab58f8852ee28c213073f8ece
Harry S <shorthouse20@gmail.com>
2023-10-25 18:44:23 +0100

    Remove now defunct morning / afternoon / evening logic to match new patterns

--- app/src/main/java/dev/shorthouse/coinwatch/ui/screen/list/CoinListViewModel.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/ui/screen/list/CoinListViewModel.kt
@@ -1,22 +1,16 @@
 package dev.shorthouse.coinwatch.ui.screen.list
 
-import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.shorthouse.coinwatch.common.Result
 import dev.shorthouse.coinwatch.domain.GetCoinsUseCase
-import dev.shorthouse.coinwatch.ui.model.TimeOfDay
-import java.time.LocalTime
 import javax.inject.Inject
-import kotlin.time.Duration.Companion.minutes
 import kotlinx.collections.immutable.toImmutableList
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 
 @HiltViewModel
@@ -34,15 +28,8 @@ class CoinListViewModel @Inject constructor(
         _uiState.update { CoinListUiState.Loading }
 
         val coinsFlow = getCoinsUseCase()
-        val currentHourFlow = flow {
-            emit(LocalTime.now().hour)
-            delay(5.minutes.inWholeMilliseconds)
-        }
 
-        combine(
-            coinsFlow,
-            currentHourFlow
-        ) { coinsResult, currentHour ->
+        coinsFlow.onEach { coinsResult ->
             when (coinsResult) {
                 is Result.Error -> {
                     _uiState.update { CoinListUiState.Error(coinsResult.message) }
@@ -50,25 +37,14 @@ class CoinListViewModel @Inject constructor(
 
                 is Result.Success -> {
                     val coins = coinsResult.data.toImmutableList()
-                    val timeOfDay = calculateTimeOfDay(currentHour)
 
                     _uiState.update {
                         CoinListUiState.Success(
-                            coins = coins,
-                            timeOfDay = timeOfDay
+                            coins = coins
                         )
                     }
                 }
             }
         }.launchIn(viewModelScope)
-    }
-
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-    fun calculateTimeOfDay(currentHour: Int): TimeOfDay {
-        return when (currentHour) {
-            in 0..11 -> TimeOfDay.Morning
-            in 12..17 -> TimeOfDay.Afternoon
-            else -> TimeOfDay.Evening
-        }
     }
 }
