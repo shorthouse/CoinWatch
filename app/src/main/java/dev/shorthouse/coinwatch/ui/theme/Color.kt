36f54c2dcfa27be9a345c6dac8f7b9f678972605
Harry S (Macbook) <shorthouse20@gmail.com>
2023-10-28 11:49:33 +0100

    Change colours to improve contrast and add new third colour

--- app/src/main/java/dev/shorthouse/coinwatch/ui/theme/Color.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/ui/theme/Color.kt
@@ -2,9 +2,11 @@ package dev.shorthouse.coinwatch.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val dark_background = Color(0xFF19284D)
+val dark_primaryContainer = Color(0xFF384670)
+val dark_onPrimaryContainer = Color(0xFFFFFFFF)
+val dark_background = Color(0xFF152241)
 val dark_onBackground = Color(0xFFFFFFFF)
-val dark_surface = Color(0xFF253667)
+val dark_surface = Color(0xFF223260)
 val dark_onSurface = Color(0xFFFFFFFF)
 val dark_onSurfaceVariant = Color(0xFFBDBDBD)
 
