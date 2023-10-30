85cf1084a9bee0f5b77f0e70045a2564533db849
Harry S (Macbook) <shorthouse20@gmail.com>
2023-10-30 19:42:32 +0000

    Add user prefs logic to get coins use case

--- app/src/main/java/dev/shorthouse/coinwatch/domain/GetCoinsUseCase.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/domain/GetCoinsUseCase.kt
@@ -1,19 +1,26 @@
 package dev.shorthouse.coinwatch.domain
 
 import dev.shorthouse.coinwatch.common.Result
+import dev.shorthouse.coinwatch.data.datastore.UserPreferencesRepository
 import dev.shorthouse.coinwatch.data.repository.coin.CoinRepository
 import dev.shorthouse.coinwatch.model.Coin
 import javax.inject.Inject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
 
 class GetCoinsUseCase @Inject constructor(
-    private val coinRepository: CoinRepository
+    private val coinRepository: CoinRepository,
+    private val userPreferencesRepository: UserPreferencesRepository
 ) {
     operator fun invoke(): Flow<Result<List<Coin>>> {
         return getCoins()
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     private fun getCoins(): Flow<Result<List<Coin>>> {
-        return coinRepository.getCoins()
+        return userPreferencesRepository.userPreferencesFlow.flatMapLatest { userPreferences ->
+            coinRepository.getCoins(coinSort = userPreferences.coinSort)
+        }
     }
 }
