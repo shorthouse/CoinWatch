444c6e04af223928e93671a276f7c5827ca5d1ae
Harry S (Macbook) <shorthouse20@gmail.com>
2023-10-31 19:10:58 +0000

    Add currency to coin network data source

--- app/src/main/java/dev/shorthouse/coinwatch/data/source/remote/CoinNetworkDataSourceImpl.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/data/source/remote/CoinNetworkDataSourceImpl.kt
@@ -1,6 +1,7 @@
 package dev.shorthouse.coinwatch.data.source.remote
 
 import dev.shorthouse.coinwatch.data.datastore.CoinSort
+import dev.shorthouse.coinwatch.data.datastore.Currency
 import dev.shorthouse.coinwatch.data.source.remote.model.CoinChartApiModel
 import dev.shorthouse.coinwatch.data.source.remote.model.CoinDetailsApiModel
 import dev.shorthouse.coinwatch.data.source.remote.model.CoinSearchResultsApiModel
@@ -13,32 +14,58 @@ class CoinNetworkDataSourceImpl @Inject constructor(
 ) : CoinNetworkDataSource {
     override suspend fun getCoins(
         coinIds: List<String>,
-        coinSort: CoinSort
+        coinSort: CoinSort,
+        currency: Currency
     ): Response<CoinsApiModel> {
-        val orderBy = when (coinSort) {
-            CoinSort.MarketCap -> "marketCap"
-            CoinSort.Price -> "price"
-            CoinSort.PriceChange24h -> "change"
-            CoinSort.Volume24h -> "24hVolume"
-        }
-
-        return coinApi.getCoins(coinIds = coinIds, orderBy = orderBy)
+        return coinApi.getCoins(
+            coinIds = coinIds,
+            orderBy = coinSort.toOrderByString(),
+            currencyUUID = currency.toCurrencyUUID()
+        )
     }
 
-    override suspend fun getCoinDetails(coinId: String): Response<CoinDetailsApiModel> {
-        return coinApi.getCoinDetails(coinId = coinId)
+    override suspend fun getCoinDetails(
+        coinId: String,
+        currency: Currency
+    ): Response<CoinDetailsApiModel> {
+        return coinApi.getCoinDetails(
+            coinId = coinId,
+            currencyUUID = currency.toCurrencyUUID()
+        )
     }
 
     override suspend fun getCoinChart(
         coinId: String,
-        chartPeriod: String
+        chartPeriod: String,
+        currency: Currency
     ): Response<CoinChartApiModel> {
-        return coinApi.getCoinChart(coinId = coinId, chartPeriod = chartPeriod)
+        return coinApi.getCoinChart(
+            coinId = coinId,
+            chartPeriod = chartPeriod,
+            currencyUUID = currency.toCurrencyUUID()
+        )
     }
 
     override suspend fun getCoinSearchResults(
         searchQuery: String
     ): Response<CoinSearchResultsApiModel> {
         return coinApi.getCoinSearchResults(searchQuery = searchQuery)
+    }
+}
+
+private fun CoinSort.toOrderByString(): String {
+    return when (this) {
+        CoinSort.MarketCap -> "marketCap"
+        CoinSort.Price -> "price"
+        CoinSort.PriceChange24h -> "change"
+        CoinSort.Volume24h -> "24hVolume"
+    }
+}
+
+private fun Currency.toCurrencyUUID(): String {
+    return when (this) {
+        Currency.USD -> "yhjMzLPhuIDl"
+        Currency.GBP -> "Hokyui45Z38f"
+        Currency.EUR -> "5k-_VTxqtCEI"
     }
 }
