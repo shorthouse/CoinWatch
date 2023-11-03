c2ed73b56fe16eefb15b352917211cf9e3585056
Harry S (Macbook) <shorthouse20@gmail.com>
2023-11-03 23:52:51 +0000

    Add cached coin to coin mapper

--- app/src/main/java/dev/shorthouse/coinwatch/data/mapper/CoinMapper.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/data/mapper/CoinMapper.kt
@@ -1,6 +1,7 @@
 package dev.shorthouse.coinwatch.data.mapper
 
 import dev.shorthouse.coinwatch.data.datastore.Currency
+import dev.shorthouse.coinwatch.data.source.local.model.CachedCoin
 import dev.shorthouse.coinwatch.data.source.remote.model.CoinsApiModel
 import dev.shorthouse.coinwatch.model.Coin
 import dev.shorthouse.coinwatch.model.Percentage
@@ -31,5 +32,40 @@ class CoinMapper @Inject constructor() {
                     .toPersistentList()
             )
         }
+    }
+
+    fun mapApiModelToCachedModel(apiModel: CoinsApiModel, currency: Currency): List<CachedCoin> {
+        val validCoins = apiModel.coinsData?.coins
+            .orEmpty()
+            .filterNotNull()
+            .filter { it.id != null }
+
+        return validCoins.map { coinApiModel ->
+            CachedCoin(
+                id = coinApiModel.id!!,
+                name = coinApiModel.name.orEmpty(),
+                symbol = coinApiModel.symbol.orEmpty(),
+                imageUrl = coinApiModel.imageUrl.orEmpty(),
+                currentPrice = Price(coinApiModel.currentPrice, currency = currency),
+                priceChangePercentage24h = Percentage(coinApiModel.priceChangePercentage24h),
+                prices24h = coinApiModel.prices24h
+                    .orEmpty()
+                    .filterNotNull()
+                    .filter { price -> price.compareTo(BigDecimal.ZERO) >= 0 }
+                    .toPersistentList()
+            )
+        }
+    }
+
+    fun mapCachedCoinToModel(cachedCoin: CachedCoin): Coin {
+        return Coin(
+            id = cachedCoin.id,
+            name = cachedCoin.name,
+            symbol = cachedCoin.symbol,
+            imageUrl = cachedCoin.imageUrl,
+            currentPrice = cachedCoin.currentPrice,
+            priceChangePercentage24h = cachedCoin.priceChangePercentage24h,
+            prices24h = cachedCoin.prices24h
+        )
     }
 }
