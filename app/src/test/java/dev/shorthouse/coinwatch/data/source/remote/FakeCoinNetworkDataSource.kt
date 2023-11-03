35e461104860ada77424b103be09bc0080df2d10
Harry S (Macbook) <shorthouse20@gmail.com>
2023-11-03 19:19:03 +0000

    Add currency into fake coin network data source

--- app/src/test/java/dev/shorthouse/coinwatch/data/source/remote/FakeCoinNetworkDataSource.kt
+++ app/src/test/java/dev/shorthouse/coinwatch/data/source/remote/FakeCoinNetworkDataSource.kt
@@ -1,5 +1,7 @@
 package dev.shorthouse.coinwatch.data.source.remote
 
+import dev.shorthouse.coinwatch.data.datastore.CoinSort
+import dev.shorthouse.coinwatch.data.datastore.Currency
 import dev.shorthouse.coinwatch.data.source.remote.model.CoinChartApiModel
 import dev.shorthouse.coinwatch.data.source.remote.model.CoinDetailsApiModel
 import dev.shorthouse.coinwatch.data.source.remote.model.CoinSearchResultsApiModel
@@ -9,21 +11,37 @@ import retrofit2.Response
 class FakeCoinNetworkDataSource(
     private val coinApi: CoinApi
 ) : CoinNetworkDataSource {
-    override suspend fun getCoins(coinIds: List<String>): Response<CoinsApiModel> {
-        return coinApi.getCoins(coinIds = coinIds)
+    override suspend fun getCoins(
+        coinIds: List<String>,
+        coinSort: CoinSort,
+        currency: Currency
+    ): Response<CoinsApiModel> {
+        return coinApi.getCoins(
+            coinIds = coinIds,
+            orderBy = coinSort.toOrderByString(),
+            currencyUUID = currency.toCurrencyUUID()
+        )
     }
 
-    override suspend fun getCoinDetails(coinId: String): Response<CoinDetailsApiModel> {
-        return coinApi.getCoinDetails(coinId = coinId)
+    override suspend fun getCoinDetails(
+        coinId: String,
+        currency: Currency
+    ): Response<CoinDetailsApiModel> {
+        return coinApi.getCoinDetails(
+            coinId = coinId,
+            currencyUUID = currency.toCurrencyUUID()
+        )
     }
 
     override suspend fun getCoinChart(
         coinId: String,
-        chartPeriod: String
+        chartPeriod: String,
+        currency: Currency
     ): Response<CoinChartApiModel> {
         return coinApi.getCoinChart(
             coinId = coinId,
-            chartPeriod = chartPeriod
+            chartPeriod = chartPeriod,
+            currencyUUID = currency.toCurrencyUUID()
         )
     }
 
@@ -31,5 +49,22 @@ class FakeCoinNetworkDataSource(
         searchQuery: String
     ): Response<CoinSearchResultsApiModel> {
         return coinApi.getCoinSearchResults(searchQuery = searchQuery)
+    }
+}
+
+private fun CoinSort.toOrderByString(): String {
+    return when (this) {
+        CoinSort.MarketCap -> "marketCap"
+        CoinSort.Price -> "price"
+        CoinSort.PriceChange24h -> "change"
+        CoinSort.Volume24h -> "24hVolume"
+    }
+}
+
+private fun Currency.toCurrencyUUID(): String {
+    return when (this) {
+        Currency.USD -> "yhjMzLPhuIDl"
+        Currency.GBP -> "Hokyui45Z38f"
+        Currency.EUR -> "5k-_VTxqtCEI"
     }
 }
