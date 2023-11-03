d3553092b77bb053d831ade0b110bf1cf0160ce9
Harry S (Macbook) <shorthouse20@gmail.com>
2023-11-03 19:17:02 +0000

    Update repository tests to include sort and currency

--- app/src/test/java/dev/shorthouse/coinwatch/data/repository/chart/CoinChartRepositoryTest.kt
+++ app/src/test/java/dev/shorthouse/coinwatch/data/repository/chart/CoinChartRepositoryTest.kt
@@ -3,6 +3,7 @@ package dev.shorthouse.coinwatch.data.repository.chart
 import com.google.common.truth.Truth.assertThat
 import dev.shorthouse.coinwatch.MainDispatcherRule
 import dev.shorthouse.coinwatch.common.Result
+import dev.shorthouse.coinwatch.data.datastore.Currency
 import dev.shorthouse.coinwatch.data.mapper.CoinChartMapper
 import dev.shorthouse.coinwatch.data.source.remote.FakeCoinApi
 import dev.shorthouse.coinwatch.data.source.remote.FakeCoinNetworkDataSource
@@ -42,6 +43,7 @@ class CoinChartRepositoryTest {
         // Arrange
         val coinId = "Qwsogvtv82FCd"
         val chartPeriod = "1d"
+        val currency = Currency.USD
 
         val expectedResult = Result.Success(
             CoinChart(
@@ -60,7 +62,8 @@ class CoinChartRepositoryTest {
         // Act
         val result = coinChartRepository.getCoinChart(
             coinId = coinId,
-            chartPeriod = chartPeriod
+            chartPeriod = chartPeriod,
+            currency = currency
         ).first()
 
         // Assert
@@ -69,60 +72,67 @@ class CoinChartRepositoryTest {
     }
 
     @Test
-    fun `When coin chart data has null values should populate these with default values and return success`() = runTest {
-        // Arrange
-        val coinId = "nullValues"
-        val chartPeriod = "1d"
+    fun `When coin chart data has null values should populate these with default values and return success`() =
+        runTest {
+            // Arrange
+            val coinId = "nullValues"
+            val chartPeriod = "1d"
+            val currency = Currency.USD
 
-        val expectedPrices = emptyList<BigDecimal>()
-        val expectedMinPriceAmount = BigDecimal.ZERO
-        val expectedMaxPriceAmount = BigDecimal.ZERO
-        val expectedPeriodPriceChangePercentageAmount = BigDecimal.ZERO
+            val expectedPrices = emptyList<BigDecimal>()
+            val expectedMinPriceAmount = BigDecimal.ZERO
+            val expectedMaxPriceAmount = BigDecimal.ZERO
+            val expectedPeriodPriceChangePercentageAmount = BigDecimal.ZERO
 
-        // Act
-        val result = coinChartRepository.getCoinChart(
-            coinId = coinId,
-            chartPeriod = chartPeriod
-        ).first()
+            // Act
+            val result = coinChartRepository.getCoinChart(
+                coinId = coinId,
+                chartPeriod = chartPeriod,
+                currency = currency
+            ).first()
 
-        // Assert
-        assertThat(result).isInstanceOf(Result.Success::class.java)
+            // Assert
+            assertThat(result).isInstanceOf(Result.Success::class.java)
 
-        assertThat((result as Result.Success).data.prices).isEqualTo(expectedPrices)
-        assertThat(result.data.minPrice.amount).isEqualTo(expectedMinPriceAmount)
-        assertThat(result.data.maxPrice.amount).isEqualTo(expectedMaxPriceAmount)
-        assertThat(result.data.periodPriceChangePercentage.amount).isEqualTo(
-            expectedPeriodPriceChangePercentageAmount
-        )
-    }
+            assertThat((result as Result.Success).data.prices).isEqualTo(expectedPrices)
+            assertThat(result.data.minPrice.amount).isEqualTo(expectedMinPriceAmount)
+            assertThat(result.data.maxPrice.amount).isEqualTo(expectedMaxPriceAmount)
+            assertThat(result.data.periodPriceChangePercentage.amount).isEqualTo(
+                expectedPeriodPriceChangePercentageAmount
+            )
+        }
 
     @Test
-    fun `When coin chart has null prices should ignore these prices and return success`() = runTest {
-        // Arrange
-        val coinId = "nullPrices"
-        val chartPeriod = "1d"
+    fun `When coin chart has null prices should ignore these prices and return success`() =
+        runTest {
+            // Arrange
+            val coinId = "nullPrices"
+            val chartPeriod = "1d"
+            val currency = Currency.USD
 
-        val expectedPrices = listOf(
-            BigDecimal("25000.89"),
-            BigDecimal("30000.47")
-        )
+            val expectedPrices = listOf(
+                BigDecimal("25000.89"),
+                BigDecimal("30000.47")
+            )
 
-        // Act
-        val result = coinChartRepository.getCoinChart(
-            coinId = coinId,
-            chartPeriod = chartPeriod
-        ).first()
+            // Act
+            val result = coinChartRepository.getCoinChart(
+                coinId = coinId,
+                chartPeriod = chartPeriod,
+                currency = currency
+            ).first()
 
-        // Assert
-        assertThat(result).isInstanceOf(Result.Success::class.java)
-        assertThat((result as Result.Success).data.prices).isEqualTo(expectedPrices)
-    }
+            // Assert
+            assertThat(result).isInstanceOf(Result.Success::class.java)
+            assertThat((result as Result.Success).data.prices).isEqualTo(expectedPrices)
+        }
 
     @Test
     fun `When coin chart returns error should return error`() = runTest {
         // Arrange
         val coinId = "Qwditjgs82FCd"
         val chartPeriod = "1d"
+        val currency = Currency.USD
 
         val expectedResult = Result.Error<CoinChartApiModel>(
             message = "Unable to fetch coin chart"
@@ -131,7 +141,8 @@ class CoinChartRepositoryTest {
         // Act
         val result = coinChartRepository.getCoinChart(
             coinId = coinId,
-            chartPeriod = chartPeriod
+            chartPeriod = chartPeriod,
+            currency = currency
         ).first()
 
         // Assert
