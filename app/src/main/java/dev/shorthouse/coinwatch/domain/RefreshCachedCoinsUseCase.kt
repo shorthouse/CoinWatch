f2d0c0679b9bfa1dc3d2d6b305fe4b21eb1c6dfd
Harry S <shorthouse20@gmail.com>
2023-11-04 11:19:13 +0000

    Delete old cached coins before adding new

--- app/src/main/java/dev/shorthouse/coinwatch/domain/RefreshCachedCoinsUseCase.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/domain/RefreshCachedCoinsUseCase.kt
@@ -24,6 +24,7 @@ class RefreshCachedCoinsUseCase @Inject constructor(
         )
 
         if (remoteCoinsResult is Result.Success) {
+            cachedCoinRepository.deleteAllCachedCoins()
             cachedCoinRepository.insertCachedCoins(remoteCoinsResult.data)
         }
 
