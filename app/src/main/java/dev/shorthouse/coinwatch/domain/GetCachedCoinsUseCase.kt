ac408b936aac6d0b27f90ba9a89ad4ce5685771b
Harry S <shorthouse20@gmail.com>
2023-11-04 11:19:31 +0000

    Swap from Coin to Cached Coin

--- app/src/main/java/dev/shorthouse/coinwatch/domain/GetCachedCoinsUseCase.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/domain/GetCachedCoinsUseCase.kt
@@ -2,18 +2,18 @@ package dev.shorthouse.coinwatch.domain
 
 import dev.shorthouse.coinwatch.common.Result
 import dev.shorthouse.coinwatch.data.repository.cachedCoin.CachedCoinRepository
-import dev.shorthouse.coinwatch.model.Coin
+import dev.shorthouse.coinwatch.data.source.local.model.CachedCoin
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 
 class GetCachedCoinsUseCase @Inject constructor(
     private val cachedCoinRepository: CachedCoinRepository
 ) {
-    operator fun invoke(): Flow<Result<List<Coin>>> {
+    operator fun invoke(): Flow<Result<List<CachedCoin>>> {
         return getCoins()
     }
 
-    private fun getCoins(): Flow<Result<List<Coin>>> {
+    private fun getCoins(): Flow<Result<List<CachedCoin>>> {
         return cachedCoinRepository.getCachedCoins()
     }
 }
