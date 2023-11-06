54cec14fb3266c4d86bc5255d760f044f7e05ff0
Harry S (Macbook) <shorthouse20@gmail.com>
2023-11-06 19:32:52 +0000

    Extract out coin local data source interface

--- app/src/main/java/dev/shorthouse/coinwatch/data/source/local/CoinLocalDataSource.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/data/source/local/CoinLocalDataSource.kt
@@ -4,31 +4,11 @@ import dev.shorthouse.coinwatch.data.source.local.model.CachedCoin
 import dev.shorthouse.coinwatch.data.source.local.model.FavouriteCoin
 import kotlinx.coroutines.flow.Flow
 
-class CoinLocalDataSource(
-    private val favouriteCoinDao: FavouriteCoinDao,
-    private val cachedCoinDao: CachedCoinDao
-) {
-    fun getFavouriteCoins(): Flow<List<FavouriteCoin>> {
-        return favouriteCoinDao.getFavouriteCoins()
-    }
-
-    fun isCoinFavourite(coinId: String): Flow<Boolean> {
-        return favouriteCoinDao.isCoinFavourite(coinId = coinId)
-    }
-
-    suspend fun insertFavouriteCoin(favouriteCoin: FavouriteCoin) {
-        favouriteCoinDao.insert(favouriteCoin)
-    }
-
-    suspend fun deleteFavouriteCoin(favouriteCoin: FavouriteCoin) {
-        favouriteCoinDao.delete(favouriteCoin)
-    }
-
-    fun getCachedCoins(): Flow<List<CachedCoin>> {
-        return cachedCoinDao.getCachedCoins()
-    }
-
-    suspend fun refreshCachedCoins(coins: List<CachedCoin>) {
-        cachedCoinDao.refreshCachedCoins(coins)
-    }
+interface CoinLocalDataSource {
+    fun getFavouriteCoins(): Flow<List<FavouriteCoin>>
+    fun isCoinFavourite(coinId: String): Flow<Boolean>
+    suspend fun insertFavouriteCoin(favouriteCoin: FavouriteCoin)
+    suspend fun deleteFavouriteCoin(favouriteCoin: FavouriteCoin)
+    fun getCachedCoins(): Flow<List<CachedCoin>>
+    suspend fun refreshCachedCoins(coins: List<CachedCoin>)
 }
