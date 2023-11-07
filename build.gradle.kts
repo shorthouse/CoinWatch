87fcf5bf573d7d3ba8d28cffc84106624a3356b1
Harry S <shorthouse20@gmail.com>
2023-11-07 19:20:05 +0000

    Remove unused protobuf dependency and setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
     id("com.google.dagger.hilt.android") version "2.48.1" apply false
     id("com.google.devtools.ksp") version "1.8.20-1.0.11" apply false
-    id("com.google.protobuf") version "0.9.4" apply false
     kotlin("plugin.serialization") version "1.8.20" apply false
 }
