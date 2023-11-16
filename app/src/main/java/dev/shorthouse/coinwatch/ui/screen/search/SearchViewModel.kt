c2727a95722b8931da255ca24c7ebd03e74a0389
Harry S (Macbook) <shorthouse20@gmail.com>
2023-11-16 18:57:37 +0000

    Cleanup search vm

--- app/src/main/java/dev/shorthouse/coinwatch/ui/screen/search/SearchViewModel.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/ui/screen/search/SearchViewModel.kt
@@ -35,7 +35,7 @@ class SearchViewModel @Inject constructor(
         initialiseUiState()
     }
 
-    fun initialiseUiState() {
+    private fun initialiseUiState() {
         snapshotFlow { searchQuery }
             .debounce(350L)
             .onEach { query ->
