f8a1eca91cdd310ee51af9022bf05a28b69c8b73
Harry S (Macbook) <shorthouse20@gmail.com>
2023-11-16 18:38:20 +0000

    User prefs no change check

--- app/src/main/java/dev/shorthouse/coinwatch/data/datastore/UserPreferencesRepository.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/data/datastore/UserPreferencesRepository.kt
@@ -5,6 +5,7 @@ import java.io.IOException
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.first
 import timber.log.Timber
 
 class UserPreferencesRepository @Inject constructor(
@@ -21,14 +22,18 @@ class UserPreferencesRepository @Inject constructor(
         }
 
     suspend fun updateCurrency(currency: Currency) {
-        userPreferencesDataStore.updateData { currentPreferences ->
-            currentPreferences.copy(currency = currency)
+        if (currency != userPreferencesFlow.first().currency) {
+            userPreferencesDataStore.updateData { currentPreferences ->
+                currentPreferences.copy(currency = currency)
+            }
         }
     }
 
     suspend fun updateCoinSort(coinSort: CoinSort) {
-        userPreferencesDataStore.updateData { currentPreferences ->
-            currentPreferences.copy(coinSort = coinSort)
+        if (coinSort != userPreferencesFlow.first().coinSort) {
+            userPreferencesDataStore.updateData { currentPreferences ->
+                currentPreferences.copy(coinSort = coinSort)
+            }
         }
     }
 }
