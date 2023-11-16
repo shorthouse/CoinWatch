cc04283b3ba8ae35ffb9f93897ff0cdc3809e114
Harry S (Macbook) <shorthouse20@gmail.com>
2023-11-16 18:40:06 +0000

    Add loading indicator component

---
+++ app/src/main/java/dev/shorthouse/coinwatch/ui/component/LoadingIndicator.kt
@@ -0,0 +1,35 @@
+package dev.shorthouse.coinwatch.ui.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.shorthouse.coinwatch.ui.theme.AppTheme
+
+@Composable
+fun LoadingIndicator(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator(
+            color = MaterialTheme.colorScheme.onBackground,
+            strokeWidth = 5.dp,
+            strokeCap = StrokeCap.Round
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LoadingIndicatorPreview() {
+    AppTheme {
+        LoadingIndicator()
+    }
+}
