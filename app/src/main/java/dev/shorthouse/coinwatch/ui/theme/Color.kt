9386eb4184fc9318333af84711b49a58a91de8f6
Harry S (Macbook) <shorthouse20@gmail.com>
2023-11-19 18:06:31 +0000

    Add white primary colour for theme

--- app/src/main/java/dev/shorthouse/coinwatch/ui/theme/Color.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package dev.shorthouse.coinwatch.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val dark_primary = Color(0xFFFFFFFF)
 val dark_primaryContainer = Color(0xFF384670)
 val dark_onPrimaryContainer = Color(0xFFFFFFFF)
 val dark_background = Color(0xFF152241)
