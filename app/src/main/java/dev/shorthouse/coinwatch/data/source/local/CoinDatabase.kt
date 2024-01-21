a400aeb3e4c224fb5ce4de472a4764c9162b52e2
Harry S <shorthouse20@gmail.com>
2024-01-21 21:40:52 +0000

    Rename favouriteCoin to favouriteCoinId

--- app/src/main/java/dev/shorthouse/coinwatch/data/source/local/CoinDatabase.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/data/source/local/CoinDatabase.kt
@@ -2,19 +2,22 @@ package dev.shorthouse.coinwatch.data.source.local
 
 import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.RenameTable
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.AutoMigrationSpec
 import dev.shorthouse.coinwatch.data.source.local.converters.ImmutableListTypeConverter
 import dev.shorthouse.coinwatch.data.source.local.converters.PercentageTypeConverter
 import dev.shorthouse.coinwatch.data.source.local.converters.PriceTypeConverter
 import dev.shorthouse.coinwatch.data.source.local.model.CachedCoin
-import dev.shorthouse.coinwatch.data.source.local.model.FavouriteCoin
+import dev.shorthouse.coinwatch.data.source.local.model.FavouriteCoinId
 
 @Database(
-    version = 2,
-    entities = [FavouriteCoin::class, CachedCoin::class],
+    version = 3,
+    entities = [FavouriteCoinId::class, CachedCoin::class],
     autoMigrations = [
-        AutoMigration(from = 1, to = 2)
+        AutoMigration(from = 1, to = 2),
+        AutoMigration(from = 2, to = 3, spec = CoinDatabase.Migration2to3::class)
     ]
 )
 @TypeConverters(
@@ -25,4 +28,7 @@ import dev.shorthouse.coinwatch.data.source.local.model.FavouriteCoin
 abstract class CoinDatabase : RoomDatabase() {
     abstract fun favouriteCoinDao(): FavouriteCoinDao
     abstract fun cachedCoinDao(): CachedCoinDao
+
+    @RenameTable(fromTableName = "FavouriteCoin", toTableName = "FavouriteCoinId")
+    internal class Migration2to3 : AutoMigrationSpec
 }
