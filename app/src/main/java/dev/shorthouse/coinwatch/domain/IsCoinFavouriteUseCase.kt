bfa0d5836d4ce63b3bd9d3b596179a19b5cce1de
Harry S <shorthouse20@gmail.com>
2024-01-21 21:41:39 +0000

    Rename favouriteCoin to favouriteCoinId

--- app/src/main/java/dev/shorthouse/coinwatch/domain/IsCoinFavouriteUseCase.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/domain/IsCoinFavouriteUseCase.kt
@@ -1,19 +1,19 @@
 package dev.shorthouse.coinwatch.domain
 
 import dev.shorthouse.coinwatch.common.Result
-import dev.shorthouse.coinwatch.data.repository.favouriteCoin.FavouriteCoinRepository
-import dev.shorthouse.coinwatch.data.source.local.model.FavouriteCoin
+import dev.shorthouse.coinwatch.data.repository.favouriteCoinId.FavouriteCoinIdRepository
+import dev.shorthouse.coinwatch.data.source.local.model.FavouriteCoinId
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 
 class IsCoinFavouriteUseCase @Inject constructor(
-    private val favouriteCoinRepository: FavouriteCoinRepository
+    private val favouriteCoinIdRepository: FavouriteCoinIdRepository
 ) {
-    operator fun invoke(favouriteCoin: FavouriteCoin): Flow<Result<Boolean>> {
-        return isCoinFavourite(favouriteCoin = favouriteCoin)
+    operator fun invoke(favouriteCoinId: FavouriteCoinId): Flow<Result<Boolean>> {
+        return isCoinFavourite(favouriteCoinId = favouriteCoinId)
     }
 
-    private fun isCoinFavourite(favouriteCoin: FavouriteCoin): Flow<Result<Boolean>> {
-        return favouriteCoinRepository.isCoinFavourite(favouriteCoin = favouriteCoin)
+    private fun isCoinFavourite(favouriteCoinId: FavouriteCoinId): Flow<Result<Boolean>> {
+        return favouriteCoinIdRepository.isCoinFavourite(favouriteCoinId = favouriteCoinId)
     }
 }
