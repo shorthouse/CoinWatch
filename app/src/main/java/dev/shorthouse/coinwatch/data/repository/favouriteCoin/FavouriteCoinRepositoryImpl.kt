718767a8e5abc44007205d92215de2b9e067dcb4
Harry S <shorthouse20@gmail.com>
2024-01-23 21:36:31 +0000

    Add favourite coin repository

---
+++ app/src/main/java/dev/shorthouse/coinwatch/data/repository/favouriteCoin/FavouriteCoinRepositoryImpl.kt
@@ -0,0 +1,74 @@
+package dev.shorthouse.coinwatch.data.repository.favouriteCoin
+
+import dev.shorthouse.coinwatch.common.Result
+import dev.shorthouse.coinwatch.data.mapper.FavouriteCoinMapper
+import dev.shorthouse.coinwatch.data.source.local.CoinLocalDataSource
+import dev.shorthouse.coinwatch.data.source.local.model.CachedCoin
+import dev.shorthouse.coinwatch.data.source.local.model.FavouriteCoin
+import dev.shorthouse.coinwatch.data.source.remote.CoinNetworkDataSource
+import dev.shorthouse.coinwatch.data.userPreferences.CoinSort
+import dev.shorthouse.coinwatch.data.userPreferences.Currency
+import dev.shorthouse.coinwatch.di.IoDispatcher
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
+import timber.log.Timber
+
+class FavouriteCoinRepositoryImpl @Inject constructor(
+    private val coinNetworkDataSource: CoinNetworkDataSource,
+    private val coinLocalDataSource: CoinLocalDataSource,
+    private val favouriteCoinMapper: FavouriteCoinMapper,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+) : FavouriteCoinRepository {
+    override suspend fun getRemoteFavouriteCoins(
+        coinIds: List<String>,
+        coinSort: CoinSort,
+        currency: Currency
+    ): Result<List<FavouriteCoin>> = withContext(ioDispatcher) {
+        try {
+            val response = coinNetworkDataSource.getFavouriteCoins(
+                coinIds = coinIds,
+                coinSort = coinSort,
+                currency = currency
+            )
+
+            val body = response.body()
+
+            if (response.isSuccessful && body?.favouriteCoinsData != null) {
+                val favouriteCoins = favouriteCoinMapper.mapApiModelToModel(
+                    body,
+                    currency = currency
+                )
+                Result.Success(favouriteCoins)
+            } else {
+                Timber.e(
+                    "getRemoteFavouriteCoins unsuccessful retrofit response ${response.message()}"
+                )
+                Result.Error("Unable to fetch network favourite coins list")
+            }
+        } catch (e: Exception) {
+            Timber.e("getRemoteFavouriteCoins error ${e.message}")
+            Result.Error("Unable to fetch network favourite coins list")
+        }
+    }
+
+    override fun getCachedFavouriteCoins(): Flow<Result<List<FavouriteCoin>>> {
+        return coinLocalDataSource.getFavouriteCoins()
+            .map { Result.Success(it) }
+            .catch { e ->
+                Timber.e("getCachedFavouriteCoins error ${e.message}")
+                Result.Error<List<CachedCoin>>("Unable to fetch cached favourite coins")
+            }
+            .flowOn(ioDispatcher)
+    }
+
+    override suspend fun updateCachedFavouriteCoins(favouriteCoins: List<FavouriteCoin>) {
+        withContext(ioDispatcher) {
+            coinLocalDataSource.updateFavouriteCoins(favouriteCoins)
+        }
+    }
+}
