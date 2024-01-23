4a4993682c25626f7c689fa2cedec80ff91e6a47
Harry S <shorthouse20@gmail.com>
2024-01-23 22:05:00 +0000

    Add get favourite coins use case

--- app/src/main/java/dev/shorthouse/coinwatch/domain/GetFavouriteCoinsUseCase.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/domain/GetFavouriteCoinsUseCase.kt
@@ -1,58 +1,19 @@
 package dev.shorthouse.coinwatch.domain
 
 import dev.shorthouse.coinwatch.common.Result
-import dev.shorthouse.coinwatch.data.repository.coin.CoinRepository
-import dev.shorthouse.coinwatch.data.repository.favouriteCoinId.FavouriteCoinIdRepository
-import dev.shorthouse.coinwatch.data.userPreferences.UserPreferencesRepository
-import dev.shorthouse.coinwatch.model.Coin
+import dev.shorthouse.coinwatch.data.repository.favouriteCoin.FavouriteCoinRepository
+import dev.shorthouse.coinwatch.data.source.local.model.FavouriteCoin
 import javax.inject.Inject
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flow
 
 class GetFavouriteCoinsUseCase @Inject constructor(
-    private val favouriteCoinIdRepository: FavouriteCoinIdRepository,
-    private val coinRepository: CoinRepository,
-    private val userPreferencesRepository: UserPreferencesRepository
+    private val favouriteCoinRepository: FavouriteCoinRepository
 ) {
-    operator fun invoke(): Flow<Result<List<Coin>>> {
+    operator fun invoke(): Flow<Result<List<FavouriteCoin>>> {
         return getFavouriteCoins()
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
-    private fun getFavouriteCoins(): Flow<Result<List<Coin>>> {
-        return favouriteCoinIdRepository.getFavouriteCoinIds()
-            .flatMapLatest { favouriteCoinIdsResult ->
-                when (favouriteCoinIdsResult) {
-                    is Result.Success -> {
-                        val favouriteCoinIds = favouriteCoinIdsResult.data.map { it.id }
-                        getCoins(favouriteCoinIds = favouriteCoinIds)
-                    }
-
-                    is Result.Error -> {
-                        flow {
-                            emit(Result.Error(favouriteCoinIdsResult.message))
-                        }
-                    }
-                }
-            }
-    }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    private fun getCoins(favouriteCoinIds: List<String>): Flow<Result<List<Coin>>> {
-        return if (favouriteCoinIds.isEmpty()) {
-            flow {
-                emit(Result.Success(emptyList()))
-            }
-        } else {
-            userPreferencesRepository.userPreferencesFlow.flatMapLatest { userPreferences ->
-                coinRepository.getCoins(
-                    coinIds = favouriteCoinIds,
-                    coinSort = userPreferences.coinSort,
-                    currency = userPreferences.currency
-                )
-            }
-        }
+    private fun getFavouriteCoins(): Flow<Result<List<FavouriteCoin>>> {
+        return favouriteCoinRepository.getCachedFavouriteCoins()
     }
 }
