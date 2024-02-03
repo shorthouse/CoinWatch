5c7f82bd4d1d7eb2002f1c69f1a71521448eecf7
Harry S <shorthouse20@gmail.com>
2024-02-03 17:25:30 +0000

    Update favourites viewmodel tests

--- app/src/test/java/dev/shorthouse/coinwatch/ui/screen/favourites/FavouritesViewModelTest.kt
+++ app/src/test/java/dev/shorthouse/coinwatch/ui/screen/favourites/FavouritesViewModelTest.kt
@@ -2,14 +2,31 @@ package dev.shorthouse.coinwatch.ui.screen.favourites
 
 import com.google.common.truth.Truth.assertThat
 import dev.shorthouse.coinwatch.MainDispatcherRule
+import dev.shorthouse.coinwatch.R
 import dev.shorthouse.coinwatch.common.Result
-import dev.shorthouse.coinwatch.domain.GetFavouriteCoinsUseCaseOld
+import dev.shorthouse.coinwatch.data.source.local.model.FavouriteCoin
+import dev.shorthouse.coinwatch.data.source.local.model.FavouriteCoinId
+import dev.shorthouse.coinwatch.data.userPreferences.UserPreferences
+import dev.shorthouse.coinwatch.domain.GetFavouriteCoinIdsUseCase
+import dev.shorthouse.coinwatch.domain.GetFavouriteCoinsUseCase
+import dev.shorthouse.coinwatch.domain.GetUserPreferencesUseCase
+import dev.shorthouse.coinwatch.domain.UpdateCachedFavouriteCoinsUseCase
+import dev.shorthouse.coinwatch.model.Percentage
+import dev.shorthouse.coinwatch.model.Price
 import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.unmockkAll
+import java.math.BigDecimal
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -24,14 +41,26 @@ class FavouritesViewModelTest {
     private lateinit var viewModel: FavouritesViewModel
 
     @RelaxedMockK
-    private lateinit var getFavouriteCoinsUseCaseOld: GetFavouriteCoinsUseCaseOld
+    private lateinit var getFavouriteCoinsUseCase: GetFavouriteCoinsUseCase
+
+    @RelaxedMockK
+    private lateinit var updateCachedFavouriteCoinsUseCase: UpdateCachedFavouriteCoinsUseCase
+
+    @RelaxedMockK
+    private lateinit var getFavouriteCoinIdsUseCase: GetFavouriteCoinIdsUseCase
+
+    @RelaxedMockK
+    private lateinit var getUserPreferencesUseCase: GetUserPreferencesUseCase
 
     @Before
     fun setup() {
         MockKAnnotations.init(this)
 
         viewModel = FavouritesViewModel(
-            getFavouriteCoinsUseCaseOld = getFavouriteCoinsUseCaseOld
+            getFavouriteCoinsUseCase = getFavouriteCoinsUseCase,
+            updateCachedFavouriteCoinsUseCase = updateCachedFavouriteCoinsUseCase,
+            getFavouriteCoinIdsUseCase = getFavouriteCoinIdsUseCase,
+            getUserPreferencesUseCase = getUserPreferencesUseCase
         )
     }
 
@@ -53,12 +82,78 @@ class FavouritesViewModelTest {
     }
 
     @Test
-    fun `When favourite coins returns error should have error UI state`() {
+    fun `When favourite coin ids returns error should add error message`() {
         // Arrange
-        val errorMessage = "Error message"
-        val expectedUiState = FavouritesUiState(errorMessage = errorMessage)
+        every {
+            getFavouriteCoinIdsUseCase()
+        } returns flowOf(Result.Error("Favourite coin IDs error"))
 
-        every { getFavouriteCoinsUseCaseOld() } returns flowOf(Result.Error(errorMessage))
+        every { getUserPreferencesUseCase() } returns flowOf(UserPreferences())
+
+        // Act
+        viewModel.initialiseUiState()
+
+        // Assert
+        assertThat(viewModel.uiState.value.errorMessageIds)
+            .contains(R.string.error_local_favourite_coin_ids)
+    }
+
+    @Test
+    fun `When update cached coins returns error should add error message`() {
+        // Arrange
+        val favouriteCoinIds = listOf(FavouriteCoinId("Bitcoin"))
+        val userPreferences = UserPreferences()
+
+        every { getFavouriteCoinIdsUseCase() } returns flowOf(Result.Success(favouriteCoinIds))
+        every { getUserPreferencesUseCase() } returns flowOf(userPreferences)
+        coEvery {
+            updateCachedFavouriteCoinsUseCase(
+                coinIds = favouriteCoinIds,
+                currency = userPreferences.currency,
+                coinSort = userPreferences.coinSort
+            )
+        } returns Result.Error("Update cached favourite coins error")
+
+        // Act
+        viewModel.initialiseUiState()
+
+        // Assert
+        assertThat(viewModel.uiState.value.errorMessageIds)
+            .contains(R.string.error_network_favourite_coins)
+    }
+
+    @Test
+    fun `When update cached coins returns success should not add error`() {
+        // Arrange
+        val favouriteCoinIds = listOf(FavouriteCoinId("Bitcoin"))
+        val userPreferences = UserPreferences()
+
+        every { getFavouriteCoinIdsUseCase() } returns flowOf(Result.Success(favouriteCoinIds))
+        every { getUserPreferencesUseCase() } returns flowOf(userPreferences)
+        coEvery {
+            updateCachedFavouriteCoinsUseCase(
+                coinIds = favouriteCoinIds,
+                currency = userPreferences.currency,
+                coinSort = userPreferences.coinSort
+            )
+        } returns Result.Success(emptyList())
+
+        // Act
+        viewModel.initialiseUiState()
+
+        // Assert
+        assertThat(viewModel.uiState.value.errorMessageIds)
+            .doesNotContain(R.string.error_network_favourite_coins)
+    }
+
+    @Test
+    fun `When get favourite coins returns error should add error message and stop loading`() {
+        // Arrange
+        val expectedUiState = FavouritesUiState(
+            isLoading = false,
+            errorMessageIds = listOf(R.string.error_local_favourite_coins)
+        )
+        every { getFavouriteCoinsUseCase() } returns flowOf(Result.Error("Favourite coins error"))
 
         // Act
         viewModel.initialiseUiState()
@@ -68,18 +163,103 @@ class FavouritesViewModelTest {
     }
 
     @Test
-    fun `When favourite coins returns success should have success UI state`() {
+    fun `When get favourite coins returns success should update coins and stop loading`() {
         // Arrange
-        val expectedUiState = FavouritesUiState(
-            favouriteCoins = persistentListOf()
+        val favouriteCoins = listOf(
+            FavouriteCoin(
+                id = "bitcoin",
+                symbol = "BTC",
+                name = "Bitcoin",
+                imageUrl = "https://cdn.coinranking.com/bOabBYkcX/bitcoin_btc.svg",
+                currentPrice = Price("29446.336548759988"),
+                priceChangePercentage24h = Percentage("1.76833"),
+                prices24h = persistentListOf(
+                    BigDecimal("29390.15178296929"),
+                    BigDecimal("29428.222505493162"),
+                    BigDecimal("29475.12359313808"),
+                    BigDecimal("29471.20179209623")
+                )
+            )
         )
 
-        every { getFavouriteCoinsUseCaseOld() } returns flowOf(Result.Success(emptyList()))
+        val expectedUiState = FavouritesUiState(
+            favouriteCoins = favouriteCoins.toPersistentList(),
+            isLoading = false,
+        )
+
+        every { getFavouriteCoinsUseCase() } returns flowOf(Result.Success(favouriteCoins))
 
         // Act
         viewModel.initialiseUiState()
 
         // Assert
         assertThat(viewModel.uiState.value).isEqualTo(expectedUiState)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `When pull refreshing favourite coins with coin ids error should add error message`() = runTest {
+        // Arrange
+        every {
+            getFavouriteCoinIdsUseCase()
+        } returns flowOf(Result.Error("Favourite coin IDs error"))
+        every { getUserPreferencesUseCase() } returns flowOf(UserPreferences())
+
+        // Act
+        viewModel.pullRefreshFavouriteCoins()
+
+        // Assert
+        assertThat(viewModel.uiState.value.isRefreshing).isTrue()
+        advanceUntilIdle()
+        assertThat(viewModel.uiState.value.isRefreshing).isFalse()
+
+        assertThat(viewModel.uiState.value.errorMessageIds)
+            .contains(R.string.error_local_favourite_coin_ids)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `When pull refreshing favourite coins with success should call update cached favourite coins`() = runTest {
+        // Arrange
+        val favouriteCoinIds = listOf(FavouriteCoinId("Bitcoin"))
+        val userPreferences = UserPreferences()
+
+        every { getFavouriteCoinIdsUseCase() } returns flowOf(Result.Success(favouriteCoinIds))
+        every { getUserPreferencesUseCase() } returns flowOf(userPreferences)
+
+        // Act
+        viewModel.pullRefreshFavouriteCoins()
+
+        // Assert
+        assertThat(viewModel.uiState.value.isRefreshing).isTrue()
+        advanceUntilIdle()
+        assertThat(viewModel.uiState.value.isRefreshing).isFalse()
+
+        coVerify {
+            updateCachedFavouriteCoinsUseCase(
+                coinIds = favouriteCoinIds,
+                currency = userPreferences.currency,
+                coinSort = userPreferences.coinSort
+            )
+        }
+
+        confirmVerified(updateCachedFavouriteCoinsUseCase)
+    }
+
+    @Test
+    fun `When dismissing error should remove specified error from list`() {
+        // Arrange
+        every { getFavouriteCoinsUseCase() } returns flowOf(Result.Error("Favourite coins error"))
+
+        // Act
+        viewModel.initialiseUiState()
+
+        val errorIsInserted = viewModel.uiState.value.errorMessageIds.isNotEmpty()
+        val errorMessageId = viewModel.uiState.value.errorMessageIds.first()
+        viewModel.dismissErrorMessage(errorMessageId)
+
+        // Assert
+        assertThat(errorIsInserted).isTrue()
+        assertThat(viewModel.uiState.value.errorMessageIds).doesNotContain(errorMessageId)
     }
 }
