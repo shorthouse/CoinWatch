b68137cb954fa2f4bc95eab051ba7006188771a6
Harry S (Macbook) <shorthouse20@gmail.com>
2024-03-11 19:24:26 +0000

    import ordering

--- app/src/main/java/dev/shorthouse/coinwatch/data/preferences/favourites/FavouritesPreferencesRepository.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/data/preferences/favourites/FavouritesPreferencesRepository.kt
@@ -1,12 +1,12 @@
 package dev.shorthouse.coinwatch.data.preferences.favourites
 
 import androidx.datastore.core.DataStore
-import java.io.IOException
-import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
 import timber.log.Timber
+import java.io.IOException
+import javax.inject.Inject
 
 class FavouritesPreferencesRepository @Inject constructor(
     private val favouritesPreferencesDataStore: DataStore<FavouritesPreferences>
