83979378f49bb383d83279dc23611f5010f80fce
Harry S (Macbook) <shorthouse20@gmail.com>
2024-03-11 18:32:47 +0000

    Add base preferences serializer

---
+++ app/src/main/java/dev/shorthouse/coinwatch/data/preferences/BasePreferencesSerializer.kt
@@ -0,0 +1,37 @@
+package dev.shorthouse.coinwatch.data.preferences
+
+import androidx.datastore.core.Serializer
+import dev.shorthouse.coinwatch.data.preferences.global.UserPreferences
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
+import timber.log.Timber
+import java.io.InputStream
+import java.io.OutputStream
+
+abstract class BasePreferencesSerializer<T>(
+    private val defaultInstance: () -> T,
+    private val serializer: KSerializer<T>,
+) : Serializer<T> {
+
+    override val defaultValue: T
+        get() = defaultInstance()
+
+    override suspend fun readFrom(input: InputStream): T {
+        return try {
+            Json.decodeFromString(serializer, input.readBytes().decodeToString())
+        } catch (exception: SerializationException) {
+            Timber.e("Error serializing ${serializer} preferences}", exception)
+            defaultValue
+        }
+    }
+
+    override suspend fun writeTo(t: T, output: OutputStream) {
+        output.write(Json.encodeToString(serializer, t).encodeToByteArray())
+    }
+}
+
+object UserPreferencesSerializer : BasePreferencesSerializer<UserPreferences>(
+    defaultInstance = { UserPreferences() },
+    serializer = UserPreferences.serializer(),
+)
