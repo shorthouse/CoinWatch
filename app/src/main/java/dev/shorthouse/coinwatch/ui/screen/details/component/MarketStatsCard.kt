e85873cba97b74183229a890817ac93549f44636
Harry S (Macbook) <shorthouse20@gmail.com>
2024-03-14 19:39:55 +0000

    Add ath caveat to details and and reorder market stats

--- app/src/main/java/dev/shorthouse/coinwatch/ui/screen/details/component/MarketStatsCard.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/ui/screen/details/component/MarketStatsCard.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.shorthouse.coinwatch.R
+import dev.shorthouse.coinwatch.data.preferences.global.Currency
 import dev.shorthouse.coinwatch.model.CoinDetails
 import dev.shorthouse.coinwatch.model.Price
 import dev.shorthouse.coinwatch.ui.theme.AppTheme
@@ -33,8 +34,20 @@ fun MarketStatsCard(
                 value = coinDetails.marketCapRank
             ),
             CoinDetailsListItem(
+                nameId = if (coinDetails.currentPrice.currency == Currency.USD) {
+                    R.string.list_item_ath
+                } else {
+                    R.string.list_item_ath_dollar_only
+                },
+                value = coinDetails.allTimeHigh.formattedAmount
+            ),
+            CoinDetailsListItem(
                 nameId = R.string.list_item_market_cap,
                 value = coinDetails.marketCap.formattedAmount
+            ),
+            CoinDetailsListItem(
+                nameId = R.string.list_item_ath_date,
+                value = coinDetails.allTimeHighDate
             ),
             CoinDetailsListItem(
                 nameId = R.string.list_item_volume_24h,
@@ -43,14 +56,6 @@ fun MarketStatsCard(
             CoinDetailsListItem(
                 nameId = R.string.list_item_circulating_supply,
                 value = coinDetails.circulatingSupply
-            ),
-            CoinDetailsListItem(
-                nameId = R.string.list_item_ath,
-                value = coinDetails.allTimeHigh.formattedAmount
-            ),
-            CoinDetailsListItem(
-                nameId = R.string.list_item_ath_date,
-                value = coinDetails.allTimeHighDate
             ),
             CoinDetailsListItem(
                 nameId = R.string.list_item_listed_date,
@@ -116,7 +121,29 @@ private fun MarketStatsCardPreview() {
                 allTimeHighDate = "10 Nov 2021",
                 listedDate = "7 Aug 2015"
             )
+        )
+    }
+}
 
+@Preview
+@Composable
+private fun MarketStatsCardPreviewNotDollar() {
+    AppTheme {
+        MarketStatsCard(
+            coinDetails = CoinDetails(
+                id = "ethereum",
+                name = "Ethereum",
+                symbol = "ETH",
+                imageUrl = "https://cdn.coinranking.com/rk4RKHOuW/eth.svg",
+                currentPrice = Price("1879.14", currency = Currency.GBP),
+                marketCap = Price("225722901094", currency = Currency.GBP),
+                marketCapRank = "2",
+                volume24h = "6,627,669,115",
+                circulatingSupply = "120,186,525",
+                allTimeHigh = Price("4878.26", currency = Currency.USD),
+                allTimeHighDate = "10 Nov 2021",
+                listedDate = "7 Aug 2015"
+            )
         )
     }
 }
