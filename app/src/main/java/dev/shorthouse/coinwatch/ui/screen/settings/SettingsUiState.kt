c4ec0f421b052a32a54e72ea7ed137c6b7d02383
Harry S (Macbook) <shorthouse20@gmail.com>
2024-03-21 20:10:18 +0000

    Add start screen bottom sheet to ui state

--- app/src/main/java/dev/shorthouse/coinwatch/ui/screen/settings/SettingsUiState.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/ui/screen/settings/SettingsUiState.kt
@@ -4,9 +4,10 @@ import dev.shorthouse.coinwatch.data.preferences.global.Currency
 import dev.shorthouse.coinwatch.data.preferences.global.StartScreen
 
 data class SettingsUiState(
-    val startScreen: StartScreen = StartScreen.Market,
     val currency: Currency = Currency.USD,
     val isCurrencySheetShown: Boolean = false,
+    val startScreen: StartScreen = StartScreen.Market,
+    val isStartScreenSheetShown: Boolean = false,
     val isLoading: Boolean = false,
     val errorMessage: String? = null
 )
