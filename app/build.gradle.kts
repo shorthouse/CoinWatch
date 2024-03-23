6467d37b318abc4d14ae5c68f33d54a162a05e1d
Harry S <shorthouse20@gmail.com>
2024-03-23 11:55:52 +0000

    Bump version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "dev.shorthouse.coinwatch"
         minSdk = 26
         targetSdk = 34
-        versionCode = 19
-        versionName = "1.2.5"
+        versionCode = 20
+        versionName = "1.2.5.1"
         ndk {
             debugSymbolLevel = "FULL"
         }
