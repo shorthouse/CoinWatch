84b91b12b2e91ade90b8678fedca895913fc4915
Harry S <shorthousedev@gmail.com>
2026-05-07 21:12:05 +0100

    Add new e2e helpers

--- app/src/androidTest/java/dev/shorthouse/coinwatch/e2e/E2EComposeTestHelpers.kt
+++ app/src/androidTest/java/dev/shorthouse/coinwatch/e2e/E2EComposeTestHelpers.kt
@@ -1,5 +1,8 @@
 package dev.shorthouse.coinwatch.e2e
 
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.isSelected
 import androidx.compose.ui.test.junit4.ComposeTestRule
 import androidx.compose.ui.test.onAllNodesWithText
 import androidx.test.core.app.ActivityScenario
@@ -15,6 +18,32 @@ fun ComposeTestRule.awaitText(
     waitUntil(timeoutMillis) {
         onAllNodesWithText(
             text = text,
+            useUnmergedTree = useUnmergedTree,
+        ).fetchSemanticsNodes().isNotEmpty()
+    }
+}
+
+fun ComposeTestRule.awaitSelectedText(
+    text: String,
+    timeoutMillis: Long = E2E_DEFAULT_TIMEOUT_MILLIS,
+    useUnmergedTree: Boolean = false,
+) {
+    waitUntil(timeoutMillis) {
+        onAllNodes(
+            matcher = hasText(text).and(isSelected()),
+            useUnmergedTree = useUnmergedTree,
+        ).fetchSemanticsNodes().isNotEmpty()
+    }
+}
+
+fun ComposeTestRule.awaitContentDescription(
+    contentDescription: String,
+    timeoutMillis: Long = E2E_DEFAULT_TIMEOUT_MILLIS,
+    useUnmergedTree: Boolean = false,
+) {
+    waitUntil(timeoutMillis) {
+        onAllNodes(
+            matcher = hasContentDescription(contentDescription),
             useUnmergedTree = useUnmergedTree,
         ).fetchSemanticsNodes().isNotEmpty()
     }
