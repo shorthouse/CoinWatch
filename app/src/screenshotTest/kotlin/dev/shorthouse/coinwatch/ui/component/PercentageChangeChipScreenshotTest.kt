647ec83d84a6ff975e64e324501d4c2474a82913
Harry S <shorthousedev@gmail.com>
2026-05-12 20:45:59 +0100

    Add initial screenshot test

---
+++ app/src/screenshotTest/kotlin/dev/shorthouse/coinwatch/ui/component/PercentageChangeChipScreenshotTest.kt
@@ -0,0 +1,22 @@
+package dev.shorthouse.coinwatch.ui.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import com.android.tools.screenshot.PreviewTest
+import dev.shorthouse.coinwatch.model.Percentage
+import dev.shorthouse.coinwatch.ui.previewdata.PercentagePreviewProvider
+import dev.shorthouse.coinwatch.ui.theme.AppTheme
+
+@PreviewTest
+@Preview(showBackground = true)
+@Composable
+fun PercentageChangeChipScreenshotTest(
+    @PreviewParameter(PercentagePreviewProvider::class) percentage: Percentage
+) {
+    AppTheme {
+        PercentageChangeChip(
+            percentage = percentage
+        )
+    }
+}
