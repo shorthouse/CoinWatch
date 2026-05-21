7bc8e85d898f15b79e17a24ec45d3d11257dc93f
Harry S <shorthousedev@gmail.com>
2026-05-21 22:38:11 +0100

    Add tweaks and format locale to Price.kt

--- app/src/main/java/dev/shorthouse/coinwatch/model/Price.kt
+++ app/src/main/java/dev/shorthouse/coinwatch/model/Price.kt
@@ -1,25 +1,29 @@
 package dev.shorthouse.coinwatch.model
 
-import dev.shorthouse.coinwatch.common.Constants.MISSING_VALUE_PLACEHOLDER
 import dev.shorthouse.coinwatch.common.toSanitisedBigDecimalOrNull
 import dev.shorthouse.coinwatch.data.source.local.datastore.global.Currency
 import java.math.BigDecimal
+import java.util.Locale
 
 data class Price(val price: String?, val currency: Currency = Currency.USD) : Comparable<Price> {
 
-    val amount: BigDecimal = price.toSanitisedBigDecimalOrNull() ?: BigDecimal.ZERO
+    val amount: BigDecimal
+        get() = price.toSanitisedBigDecimalOrNull() ?: BigDecimal.ZERO
 
-    val formattedAmount: String = formatAmount()
+    val formattedAmount: String
+        get() = formatAmount()
 
     private fun formatAmount(): String {
+        val formatLocale = Locale.getDefault(Locale.Category.FORMAT)
         val amountToFormat = price.toSanitisedBigDecimalOrNull()
 
         return if (amountToFormat == null) {
-            "${currency.symbol}$MISSING_VALUE_PLACEHOLDER"
+            PriceFormatter.formatMissing(currency = currency, formatLocale = formatLocale)
         } else {
             PriceFormatter.format(
                 amount = amountToFormat,
-                currency = currency
+                currency = currency,
+                formatLocale = formatLocale
             )
         }
     }
