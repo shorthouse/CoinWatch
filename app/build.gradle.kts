85579a10947a8c170ff8ed8b82b46123ee80e854
Harry S <shorthousedev@gmail.com>
2026-06-23 14:03:12 +0100

    Bump targetSdk 36 -> 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     defaultConfig {
         applicationId = "dev.shorthouse.coinwatch"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 28
         versionName = "1.5.0"
         ndk {
